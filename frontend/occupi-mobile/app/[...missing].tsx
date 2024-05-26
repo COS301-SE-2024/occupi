686a6129dcca7966ab50f3a719781304dc1b918c
cmokou <klifo14@gmail.com>
2024-05-26 09:39:07 +0200

    refactor: Remove unused test files and components, and refactor layout
    Simplify the codebase by removing unused test files and components, including ThemedText-test.tsx, StyledText-test.js, and the Themed, ParallaxScrollView, HelloWave, EditScreenInfo, Collapsible, and HomeScreen components. Refactor the GuestLayout component to improve code organization and readability. These changes improve maintainability and simplify the codebase.

--- frontend/occupi-mobile/app/[...missing].tsx
+++ frontend/occupi-mobile/app/[...missing].tsx
@@ -1,22 +1,20 @@
-import React from 'react';
 import { Link, Stack } from 'expo-router';
 import { StyleSheet } from 'react-native';
+
 import { Text, View } from '../components/Themed';
-import { StyledProvider } from '@gluestack-style/react'; // Import StyledProvider
 
 export default function NotFoundScreen() {
   return (
-    <StyledProvider config={{}}> {/* Pass an empty object as the config */}
-      <>
-        <Stack.Screen options={{ title: 'Oops!' }} />
-        <View style={styles.container}>
-          <Text style={styles.title}>This screen doesn't exist.</Text>
-          <Link href="/" style={styles.link}>
-            <Text style={styles.linkText}>Go to home screen!</Text>
-          </Link>
-        </View>
-      </>
-    </StyledProvider>
+    <>
+      <Stack.Screen options={{ title: 'Oops!' }} />
+      <View style={styles.container}>
+        <Text style={styles.title}>This screen doesn't exist.</Text>
+
+        <Link href="/" style={styles.link}>
+          <Text style={styles.linkText}>Go to home screen!</Text>
+        </Link>
+      </View>
+    </>
   );
 }
 
