0ede8f7d69f6715808c6037c3406dc2f652a41c3
cmokou <klifo14@gmail.com>
2024-05-26 09:39:58 +0200

    refactor: Remove unused StyledProvider component from verify-otp.tsx
    Simplify the codebase by removing the unused StyledProvider component from the verify-otp.tsx file. This component was not being used and can be safely removed. These changes improve code cleanliness and remove unnecessary dependencies.

--- frontend/occupi-mobile/app/verify-otp.tsx
+++ frontend/occupi-mobile/app/verify-otp.tsx
@@ -1,12 +1,7 @@
 import OtpVerificationScreen from "../screens/Login/OtpVerification";
 
-import React from 'react';
-import { StyledProvider } from '@gluestack-style/react'; // Import StyledProvider
-
 export default function Home() {
   return (
-    <StyledProvider config={undefined}>
-      <OtpVerificationScreen />
-    </StyledProvider>
+    <OtpVerificationScreen />
   );
 }
