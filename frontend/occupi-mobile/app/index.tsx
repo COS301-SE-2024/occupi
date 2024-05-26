cedd43bf5f4280bfbcd917bedc63ed69e9cafce0
cmokou <klifo14@gmail.com>
2024-05-26 09:39:32 +0200

    refactor: Simplify layout and remove unused test files and components

--- frontend/occupi-mobile/app/index.tsx
+++ frontend/occupi-mobile/app/index.tsx
@@ -1,12 +1,5 @@
 import HomeScreen from '../screens/Login/SplashScreen';
 
-import React from 'react';
-import { StyledProvider } from '@gluestack-style/react'; // Import StyledProvider
-
 export default function Home() {
-  return (
-    <StyledProvider config={undefined}>
-      <HomeScreen />
-    </StyledProvider>
-  );
+  return <HomeScreen />;
 }
