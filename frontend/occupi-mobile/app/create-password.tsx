5287fad99a1e6d749ae01e3f5adb7531f1e60b63
cmokou <klifo14@gmail.com>
2024-05-26 09:39:20 +0200

    refactor: Remove unused test files and components, and refactor layout
    Simplify the codebase by removing unused test files and components, including ThemedText-test.tsx, StyledText-test.js, and the Themed, ParallaxScrollView, HelloWave, EditScreenInfo, Collapsible, and HomeScreen components. Refactor the GuestLayout component to improve code organization and readability. These changes improve maintainability and simplify the codebase.

--- frontend/occupi-mobile/app/create-password.tsx
+++ frontend/occupi-mobile/app/create-password.tsx
@@ -1,12 +1,7 @@
 import CreatePasswordScreen from "../screens/Login/CreatePassword";
 
-import React from 'react';
-import { StyledProvider } from '@gluestack-style/react'; // Import StyledProvider
-
 export default function Home() {
   return (
-    <StyledProvider config={undefined}>
-      <CreatePasswordScreen />
-    </StyledProvider>
+    <CreatePasswordScreen />
   );
 }
