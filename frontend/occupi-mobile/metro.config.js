934e8baa40ea31907d0b9d039194dd5300649fe5
cmokou <klifo14@gmail.com>
2024-05-26 09:30:53 +0200

    chore: Add @gluestack-style/babel-plugin-styled-resolver plugin and update metro.config.js and package.json

---
+++ frontend/occupi-mobile/metro.config.js
@@ -0,0 +1,12 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname, {
+  // [Web-only]: Enables CSS support in Metro.
+  isCSSEnabled: true,
+});
+
+config.resolver.sourceExts = [...config.resolver.sourceExts, "mjs", "cjs"];
+
+module.exports = config;
