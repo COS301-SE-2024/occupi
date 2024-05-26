312277fc17aa4033feb725f02809f32bae265930
cmokou <klifo14@gmail.com>
2024-05-26 09:39:26 +0200

    refactor: Remove unused test files and components, and refactor layout

--- frontend/occupi-mobile/app/forgot-password.tsx
+++ frontend/occupi-mobile/app/forgot-password.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
-import { StyledProvider } from '@gluestack-style/react'; // Import StyledProvider
-import ForgotPasswordScreen from '../screens/Login/ForgotPassword';
+import ForgotPasswordScreen from "../screens/Login/ForgotPassword";
 
 export default function Home() {
   return (
-    <StyledProvider config={undefined}>
-      <ForgotPasswordScreen />
-    </StyledProvider>
+    <ForgotPasswordScreen />
   );
 }
