04dd612f01de0269458a080de94a9dcbf810c757
cmokou <klifo14@gmail.com>
2024-05-26 09:39:37 +0200

    refactor: Remove unused StyledProvider component and update login screen
    Simplify the codebase by removing the unused StyledProvider component from the login.tsx file. This component was not being used and can be safely removed. Additionally, update the login screen to remove the unnecessary wrapping of the LoginScreen component with the StyledProvider component. These changes improve code cleanliness and remove unnecessary dependencies.

--- frontend/occupi-mobile/app/login.tsx
+++ frontend/occupi-mobile/app/login.tsx
@@ -1,12 +1,7 @@
 import LoginScreen from "../screens/Login/SignIn";
 
-import React from 'react';
-import { StyledProvider } from '@gluestack-style/react'; // Import StyledProvider
-
 export default function Home() {
   return (
-    <StyledProvider config={undefined}>
-      <LoginScreen />
-    </StyledProvider>
+    <LoginScreen />
   );
 }
