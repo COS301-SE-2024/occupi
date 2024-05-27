f4d94c54295b2d8646ce64c0e27aaef3dba707ee
Michael <u21546551@tuks.co.za>
2024-05-27 15:24:55 +0200

    chore: Rename nginx service to occupi-nginx in docker-compose.yml

---
+++ occupi-backend/tests/handlers_test.go
@@ -0,0 +1,43 @@
+package tests
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/joho/godotenv"
+
+	"github.com/COS301-SE-2024/occupi/occupi-backend/pkg/database"
+	"github.com/COS301-SE-2024/occupi/occupi-backend/pkg/handlers"
+)
+
+func TestGetResource(t *testing.T) {
+	// Load environment variables from .env file
+	if err := godotenv.Load("../.env"); err != nil {
+		log.Fatal(fmt.Printf("Error loading .env file %s", err))
+	}
+
+	//connect to the database
+	db := database.ConnectToDatabase()
+
+	req, err := http.NewRequest("GET", "/api/resource", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(handlers.FetchResource(db))
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	expected := `{"message":"Hello, World!"}`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+}
