64a8a69683dc579ba85adcf3fb3ea185489357f0
Tinashe-Austin <u21564176@tuks.co.za>
2024-05-28 20:48:37 +0200

    edited css and html to accomodate tailwind

--- frontend/occupi-web/src/Components/gradientButtonComponent/gradientButton.tsx
+++ frontend/occupi-web/src/Components/gradientButtonComponent/gradientButton.tsx
@@ -2,16 +2,18 @@ import React from 'react'
 import './gradientButton.css'
 
 interface GradientButtonProps {
-    buttonText: string;
+  buttonText: string;
+  containerClassName?: string;
+  buttonClassName?: string; 
   }
 
-  const GradientButton: React.FC<GradientButtonProps> = ({ buttonText }) => {
+  const GradientButton: React.FC<GradientButtonProps> = ({ buttonText ,containerClassName,buttonClassName}) => {
     return (
-      <div className="box">
-        <button className="login-button">
-          <div className="text-wrapper-9">{buttonText}</div>
-        </button>
-      </div>
+      <div className={`flex flex-col h-screen justify-end items-center pb-10 ${containerClassName}`}>
+      <button className={`bg-colours-linear-gradient text-white font-semibold py-3 px-48 text-xl rounded-lg shadow-xl hover:opacity-90 transition-opacity duration-300 ${buttonClassName}`}>
+        {buttonText}
+      </button>
+    </div>
     );
   };
   
