3d4c38bf851fc8d54cc1104302a7225a21f9fbc8
Tinashe-Austin <u21564176@tuks.co.za>
2024-05-31 01:29:48 +0200

    Page testing

--- frontend/occupi-web/src/App.tsx
+++ frontend/occupi-web/src/App.tsx
@@ -2,17 +2,24 @@ import { useState } from 'react'
 import './App.css'
 import LoginForm from './pages/Login/LoginForm'
 import OtpPage from './pages/otp-page/OtpPage'
-
-
+import SideNav from './Components/sideNavComponent/SideNav'
+import LandingPage from './pages/landing-page/LandingPage'
+import TabComponent from './Components/tabComponent/TabComponent'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     
       <div>
-       <LoginForm />
+       {/* <LoginForm /> */}
 {/* <OtpComponent /> */}
 {/* <OtpPage /> */}
+{/* <SideNav /> */}
+<LandingPage />
+{/* <div className='ml-5'>
+<TabComponent />
+</div> */}
+
     </div>
   )
 }
