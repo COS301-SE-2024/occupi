eee0911ed4b4801199a8257a7aa32409eec28ed9
Tinashe-Austin <u21564176@tuks.co.za>
2024-06-01 11:04:19 +0200

    Adding Routes

--- frontend/occupi-web/src/App.tsx
+++ frontend/occupi-web/src/App.tsx
@@ -4,19 +4,42 @@ import OtpPage from './pages/otp-page/OtpPage'
 import SideNav from './Components/sideNavComponent/SideNav'
 import LandingPage from './pages/landing-page/LandingPage'
 import TabComponent from './Components/tabComponent/TabComponent'
+import Routers from './Routers'
+import SearchBar from './Components/searchBarComponent/SearchBar'
+import Settings from './pages/settings-page/Settings'
+import Layout from './Layout'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
 
   return (
+
+    
     
       <div>
+        {/* <Routers /> */}
        {/* <LoginForm /> */}
 {/* <OtpComponent /> */}
 {/* <OtpPage /> */}
 {/* <SideNav /> */}
-<LandingPage />
+{/* <LandingPage /> */}
 {/* <div className='ml-5'>
-<TabComponent />
+
 </div> */}
+{/* <TabComponent /> */}
+
+{/* <SearchBar /> */}
+<Settings />
+
+{/* <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<LandingPage />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
+      </Routes>
+    </Router> */}
+
+
 
     </div>
   )
