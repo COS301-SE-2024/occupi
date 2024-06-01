f228770f256c6d2c0552944c640939d5027b76b3
Tinashe-Austin <u21564176@tuks.co.za>
2024-06-01 11:06:20 +0200

    OTP Edits

--- frontend/occupi-web/src/pages/otp-page/OtpPage.tsx
+++ frontend/occupi-web/src/pages/otp-page/OtpPage.tsx
@@ -1,12 +1,20 @@
 import "./OtpPage.css";
 import OtpComponent from "../../Components/OtpComponent/OtpComponent";
-import loginImage from '../../assets/otp.png';
+import loginImage from "../../assets/otp.png";
 import GradientButton from "../../Components/gradientButtonComponent/gradientButton";
 
 const OtpPage = () => {
   return (
-    <div className="box">
+    <div className="box absolute">
+       <div className="image-container w-[900px] h-[500px]">
+          <img
+            className="image w-[600px] h-[400px] relative top-[200px] right-[900px]"
+            src={loginImage}
+            alt="OTP"
+          />
+        </div>
       <div className="group">
+     
         <div className="overlap">
           <img
             className="frame"
@@ -15,22 +23,16 @@ const OtpPage = () => {
           />
           <div className="div">
             <div className="text-wrapper">We sent you an email with a code</div>
-            <div className="text-wrapper-2">Please enter the code below to continue</div>
+            <div className="text-wrapper-2">
+              Please enter the code below to continue
+            </div>
           </div>
         </div>
-        <div className="group-2">
-            <OtpComponent />
-        </div>
-        
-        <div className="button-container">
-        <GradientButton buttonText="Verify"/>
-        </div>
-
+        <div className="group-2 justify flex flex-col gap-16">
+          <OtpComponent />
+          <GradientButton buttonText="Verify" />
+        </div>     
       </div>
-
-      <div className='image-container'>
-            <img className='image' src= {loginImage} alt="OTP" />
-</div>
     </div>
   );
 };
