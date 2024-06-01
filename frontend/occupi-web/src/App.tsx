2fa7b44250c291c992794f3e384f3ba684bf38b7
Tinashe-Austin <u21564176@tuks.co.za>
2024-06-01 17:39:03 +0200

    Refactor App component and import statements

--- frontend/occupi-web/src/App.tsx
+++ frontend/occupi-web/src/App.tsx
@@ -1,36 +1,32 @@
-import './App.css'
-import LoginForm from './pages/Login/LoginForm'
-import OtpPage from './pages/otp-page/OtpPage'
-import SideNav from './Components/sideNavComponent/SideNav'
-import LandingPage from './pages/landing-page/LandingPage'
-import TabComponent from './Components/tabComponent/TabComponent'
-import Routers from './Routers'
-import SearchBar from './Components/searchBarComponent/SearchBar'
-import Settings from './pages/settings-page/Settings'
-import Layout from './Layout'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import LoginForm from "./pages/Login/LoginForm";
+import OtpPage from "./pages/otp-page/OtpPage";
+import SideNav from "./Components/sideNavComponent/SideNav";
+import LandingPage from "./pages/landing-page/LandingPage";
+import TabComponent from "./Components/tabComponent/TabComponent";
+import Routers from "./Routers";
+import SearchBar from "./Components/searchBarComponent/SearchBar";
+import Settings from "./pages/settings-page/Settings";
+import Layout from "./Layout";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
-
   return (
-
-    
-    
-      <div>
-        {/* <Routers /> */}
-       {/* <LoginForm /> */}
-{/* <OtpComponent /> */}
-{/* <OtpPage /> */}
-{/* <SideNav /> */}
-{/* <LandingPage /> */}
-{/* <div className='ml-5'>
+    <div>
+      {/* <Routers /> */}
+      {/* <LoginForm /> */}
+      {/* <OtpComponent /> */}
+      {/* <OtpPage /> */}
+      {/* <SideNav /> */}
+      <LandingPage />
+      {/* <div className='ml-5'>
 
 </div> */}
-{/* <TabComponent /> */}
+      {/* <TabComponent /> */}
 
-{/* <SearchBar /> */}
-<Settings />
+      {/* <SearchBar /> */}
+      {/* <Settings /> */}
 
-{/* <Router>
+      {/* <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<LandingPage />} />
@@ -38,11 +34,8 @@ function App() {
         </Route>
       </Routes>
     </Router> */}
-
-
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
