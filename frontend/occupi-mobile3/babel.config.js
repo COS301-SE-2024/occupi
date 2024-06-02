2be5d59d41aa3916189e91c55890e344406845a7
cmokou <klifo14@gmail.com>
2024-06-02 22:45:55 +0200

    feat: Add lazy loading for images and update Babel configuration

--- frontend/occupi-mobile3/babel.config.js
+++ frontend/occupi-mobile3/babel.config.js
@@ -1,6 +1,27 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      'babel-preset-expo',
+      '@babel/preset-react',
+      ['@babel/preset-env', { targets: { node: 'current' } }],
+      '@babel/preset-typescript',
+    ],
+    plugins: [
+      'react-native-reanimated/plugin', 
+    ],
+    env: {
+      production: {
+        plugins: ['react-native-paper/babel'],
+      },
+    },
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+    },
   };
 };
