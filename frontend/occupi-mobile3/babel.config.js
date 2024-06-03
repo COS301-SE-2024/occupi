290be0e1772fd566a6914c3862d96a6e8544e124
cmokou <klifo14@gmail.com>
2024-06-03 02:33:07 +0200

    chore: Update Babel configuration for frontend/occupi-mobile3

--- frontend/occupi-mobile3/babel.config.js
+++ frontend/occupi-mobile3/babel.config.js
@@ -1,27 +1,17 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
       'babel-preset-expo',
+      ['@babel/preset-env', { targets: { node: 'current' }, loose: true }],
       '@babel/preset-react',
-      ['@babel/preset-env', { targets: { node: 'current' } }],
       '@babel/preset-typescript',
     ],
     plugins: [
-      'react-native-reanimated/plugin', 
+      ['@babel/plugin-transform-class-properties', { loose: true }],
+      ['@babel/plugin-transform-private-methods', { loose: true }],
+      ['@babel/plugin-transform-private-property-in-object', { loose: true }],
     ],
-    env: {
-      production: {
-        plugins: ['react-native-paper/babel'],
-      },
-    },
-    transformer: {
-      getTransformOptions: async () => ({
-        transform: {
-          experimentalImportSupport: false,
-          inlineRequires: true,
-        },
-      }),
-    },
   };
 };
+
---
+++ frontend/occupi-mobile3/jestSetup.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import 'react-native-gesture-handler/jestSetup';
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+import { Animated } from 'react-native';
+
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+jest.mock('expo-linear-gradient', () => {
+  const { View } = require('react-native');
+  return {
+    LinearGradient: View,
+  };
+});
+
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
+  Reanimated.default.call = jest.fn();
+  return Reanimated;
+});
+
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper', () => ({
+  addWhitelistedNativeProps: jest.fn(),
+  addWhitelistedUIProps: jest.fn(),
+  createAnimatedComponent: (Component) => Component,
+  flushQueue: jest.fn(),
+  generateNewAnimationId: jest.fn(),
+  getCurrentTime: jest.fn(),
+  getNativeAnimationModule: jest.fn(),
+  getNativeAnimationProps: jest.fn(),
+  getNodeHandler: jest.fn(),
+  manageChildren: jest.fn(),
+  startAnimatingNode: jest.fn(),
+  stopAnimation: jest.fn(),
+}));
