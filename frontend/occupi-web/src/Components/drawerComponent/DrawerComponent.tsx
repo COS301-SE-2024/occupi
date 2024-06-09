cc71833ef5444f927dd4a442f5e956eb5ecfc746
Tinashe-Austin <tinashe.austin@tuks.co.za>
2024-06-09 22:50:35 +0200

    chore: Update .anima configuration files and package dependencies

--- frontend/occupi-web/src/Components/drawerComponent/DrawerComponent.tsx
+++ frontend/occupi-web/src/Components/drawerComponent/DrawerComponent.tsx
@@ -1,85 +1,122 @@
+import { motion } from "framer-motion";
+import { FiUser, FiImage, FiLock, FiHelpCircle, FiInfo } from "react-icons/fi";
+import MenuItem from "./menuItem/MenuItem"; // Make sure to adjust the path according to your directory structure
+import profileIcon from "../../assets/images/userprofile.svg";
+import appearanceIcon from "../../assets/images/palette.svg";
+import privacyIcon from "../../assets/images/shieldplus.svg";
+import helpIcon from "../../assets/images/alertcircle.svg";
+import aboutIcon from "../../assets/images/marker-03.svg";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { Appearance } from "@components/index";
+import { SettingsImg } from "@assets/index";
 interface DrawerComponentProps {
   isOpen: boolean;
   onClose: () => void;
-
 }
 
-const DrawerComponent = ({ isOpen, onClose }: DrawerComponentProps) => {
+const DrawerComponent = () => {
+  const navigate = useNavigate();
+  const [selectedItem, setSelectedItem] = useState<string | null>(null);
+  const [showAppearance, setShowAppearance] = useState(false);
+
+  const handleClick = (path: string) => {
+    setSelectedItem(path);
+    if (path === "/appearance") {
+      setShowAppearance(true);
+    } else {
+      setShowAppearance(false);
+    }
+  };
+
   return (
-    <div className={`drawer drawer-end ${isOpen ? 'drawer-open' : ''}`}>
-      <input id="my-drawer" type="checkbox" className="drawer-toggle" checked={isOpen} readOnly />
-      <div className="drawer-content">
-        {/* Page content here */}
-      </div>
-      <div className="drawer-side">
-        <label htmlFor="my-drawer" className="drawer-overlay" onClick={onClose}></label>
-        <div className="menu p-4 w-80 bg-base-100 text-base-content">
-          <div className="menu-title">
-            <span className="text-lg font-bold">Menu</span>
-          </div>
-          <ul className="menu-items">
+    <div className="flex flex-row relative">
+      <motion.div
+        className="w-full  z-10"
+        initial={{ x: "-1%" }}
+        animate={{ x: 20 }}
+        transition={{ duration: 0.2, ease: "linear" }}
+      >
+        <div className="menu p-4 w-80 bg-base-100 text-base-content bg-transparent">
+          <ul className="menu-items ">
             <li>
-              <a className="flex items-center gap-4 p-2 rounded-lg hover:bg-gray-200">
-                <div className="w-6 h-6">
-                  <img
-                    src="../assets/images/e7224eda-d51b-473a-9d51-d4aeb56ebbbb.png"
-                    alt="Profile Icon"
-                    className="w-full h-full object-cover"
-                  />
-                </div>
+              <motion.a
+                className={`flex items-center gap-4 p-2 rounded-lg ${
+                  selectedItem === "/profile" ? "bg-gray-200" : ""
+                }`}
+                onClick={() => handleClick("/profile")}
+                whileTap={{ scale: 0.95 }}
+              >
+                <img src={profileIcon} alt="" />
                 <span className="text-base font-medium">Profile</span>
-              </a>
+              </motion.a>
             </li>
             <li>
-              <a className="flex items-center gap-4 p-2 rounded-lg bg-gray-200 hover:bg-gray-300">
-                <div className="w-6 h-6">
-                  <img
-                    src="../assets/images/bb86e214-20d9-49e1-81df-85eb53449fda.png"
-                    alt="Appearance Icon"
-                    className="w-full h-full object-cover"
-                  />
-                </div>
+              <motion.a
+                className={`flex items-center gap-4 p-2 rounded-lg ${
+                  selectedItem === "/appearance" ? "bg-gray-200" : ""
+                }`}
+                onClick={() => handleClick("/appearance")}
+                whileTap={{ scale: 0.95 }}
+              >
+                <img src={appearanceIcon} alt="" />
                 <span className="text-base font-medium">Appearance</span>
-              </a>
+              </motion.a>
             </li>
             <li>
-              <a className="flex items-center gap-4 p-2 rounded-lg hover:bg-gray-200">
-                <div className="w-6 h-6">
-                  <img
-                    src="../assets/images/17d9a8a6-6bd2-479d-94d4-5c7aab96b5b2.png"
-                    alt="Privacy Icon"
-                    className="w-full h-full object-cover"
-                  />
-                </div>
+              <motion.a
+                className={`flex items-center gap-4 p-2 rounded-lg ${
+                  selectedItem === "/privacy" ? "bg-gray-200" : ""
+                }`}
+                onClick={() => handleClick("/privacy")}
+                whileTap={{ scale: 0.95 }}
+              >
+                <img src={privacyIcon} alt="" />
                 <span className="text-base font-medium">Privacy</span>
-              </a>
+              </motion.a>
             </li>
             <li>
-              <a className="flex items-center gap-4 p-2 rounded-lg hover:bg-gray-200">
-                <div className="w-6 h-6">
-                  <img
-                    src="../assets/images/b39442ab-9a28-412f-bb3e-60895b0890f7.png"
-                    alt="Help Icon"
-                    className="w-full h-full object-cover"
-                  />
-                </div>
+              <motion.a
+                className={`flex items-center gap-4 p-2 rounded-lg ${
+                  selectedItem === "/help" ? "bg-gray-200" : ""
+                }`}
+                onClick={() => handleClick("/help")}
+                whileTap={{ scale: 0.95 }}
+              >
+                <img src={helpIcon} alt="" />
                 <span className="text-base font-medium">Help</span>
-              </a>
+              </motion.a>
             </li>
             <li>
-              <a className="flex items-center gap-4 p-2 rounded-lg hover:bg-gray-200">
-                <div className="w-6 h-6">
-                  <img
-                    src="../assets/images/7f575c6a-f072-4174-874d-db95967b442d.png"
-                    alt="About Icon"
-                    className="w-full h-full object-cover"
-                  />
-                </div>
+              <motion.a
+                className={`flex items-center gap-4 p-2 rounded-lg ${
+                  selectedItem === "/about" ? "bg-gray-200" : ""
+                }`}
+                onClick={() => handleClick("/about")}
+                whileTap={{ scale: 0.95 }}
+              >
+                <img src={aboutIcon} alt="" />
                 <span className="text-base font-medium">About</span>
-              </a>
+              </motion.a>
             </li>
           </ul>
         </div>
+      </motion.div>
+      <div className=" absolute left-0  top-0">
+        {showAppearance ? (
+          <Appearance />
+        ) : (
+          <div className="flex flex-col items-center ml-96 mt-16 ">
+            <span className=" font-serif font-semibold text-3xl">Please Select a Setting</span>
+            <img
+              src={SettingsImg}
+              alt="Settings"
+              className="object-scale-down  "
+              
+            />
+            
+          </div>
+        )}
       </div>
     </div>
   );
