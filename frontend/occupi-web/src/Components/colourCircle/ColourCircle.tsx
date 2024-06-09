d76858f957521dbb8a6803c452c95dfc6fcc68bd
Tinashe-Austin <tinashe.austin@tuks.co.za>
2024-06-09 22:50:28 +0200

    feat: Add ColourCircle component

---
+++ frontend/occupi-web/src/Components/colourCircle/ColourCircle.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ColourCircle = () => {
+  return (
+    <div className='w-10 h-10'>
+        <div className='fixed w-10 h-10 rounded-[20px] bg-red-600'></div>
+    </div>
+  )
+}
+
+export default ColourCircle
