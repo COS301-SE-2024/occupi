cf7d9c47c00e8a414df07ffe25adfc49287180a7
Tinashe-Austin <tinashe.austin@tuks.co.za>
2024-06-11 08:12:45 +0200

    feat: Update GraphContainer to include mainComponent prop

--- frontend/occupi-web/src/Components/graphContainer/GraphContainer.tsx
+++ frontend/occupi-web/src/Components/graphContainer/GraphContainer.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 interface GraphContainerProps {
   width?: string;
   height?: string;
+  mainComponent?: JSX.Element;
 }
 
-const GraphContainer: React.FC<GraphContainerProps> = ({ width = '24.531vw', height = '13.49vw' }) => {
+const GraphContainer: React.FC<GraphContainerProps> = ({ width = '24.531vw', height = '13.49vw' ,mainComponent}) => {
   return (
     <div>
-      <div style={{ width, height }} data-testid="graph-container" className={`card w-[${width}] h-[${height}] bg-secondary rounded-[20px]`} />
+      <div style={{ width, height }} data-testid="graph-container" className={`card w-[${width}] h-[${height}] bg-transparent shadow-2xl rounded-[20px]`} >      {mainComponent}
+      </div>
     </div>
   );
 }
