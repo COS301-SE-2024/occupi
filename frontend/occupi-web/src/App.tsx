7d3976bbba531dd26c53f4888afe1be692fb82e2
Tinashe-Austin <tinashe.austin@tuks.co.za>
2024-06-11 08:12:27 +0200

    chore: Update NextUIProvider in App.tsx and tailwind.config.js

--- frontend/occupi-web/src/App.tsx
+++ frontend/occupi-web/src/App.tsx
@@ -1,10 +1,11 @@
-import { LoginForm, OtpPage, Settings, Dashboard} from "@pages/index";
+import { LoginForm, OtpPage, Settings, Dashboard,OverView} from "@pages/index";
 import {Appearance} from "@components/index";
 import { Layout } from "@layouts/index";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import {NextUIProvider} from "@nextui-org/react";
 function App() {
   return (
+<NextUIProvider>
     <Router>
       <Routes>
         <Route path="/" element={<LoginForm />} />
@@ -14,14 +15,17 @@ function App() {
           <Layout>
           <Routes>
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/overview" element={<OverView/>} />
             <Route path="settings/*" element={<Settings />}>
               <Route path="appearance" element={<Appearance />} />
+
             </Route>
           </Routes>
         </Layout>}>
         </Route>
       </Routes>
     </Router> 
+</NextUIProvider>
   )
 }
 
--- frontend/occupi-web/tailwind.config.js
+++ frontend/occupi-web/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const {nextui} = require("@nextui-org/react");
+
 module.exports = {
   darkMode: ["class"],
   content: [
@@ -6,6 +8,8 @@ module.exports = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+
   ],
   prefix: "",
   theme: {
@@ -31,6 +35,8 @@ module.exports = {
         gray_900: 'var(--tertiary)',
       },
     },
+    darkMode: "class",
+
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"),nextui(),require('daisyui')],
 }
