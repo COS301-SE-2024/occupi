d37236de4256844016d2f38696c49796cd98d505
Michael <u21546551@tuks.co.za>
2024-06-16 19:28:07 +0200

    chore: Update Docker image tag and file path in deploy-golang-develop.yml

--- documentation/occupi-docs/next.config.js
+++ documentation/occupi-docs/next.config.js
@@ -3,28 +3,7 @@ const withNextra = require('nextra')({
     themeConfig: './theme.config.jsx'
   })
 
-const isDev = process.env.NODE_ENV === 'development';
-const isBackend = process.env.APP_ENV === 'backend';
-
-const prodConfig = {
-  basePath: '/occupi',
-  assetPrefix: '/occupi/',
-  output: 'export',
-  images: {
-    unoptimized: true
-  }
-};
-
-const backendConfig = {
-  basePath: '/documentation',
-  assetPrefix: '/documentation/',
-  output: 'export',
-  images: {
-    unoptimized: true
-  }
-}
-
-module.exports = withNextra(isDev ?  {} : isBackend ? backendConfig : prodConfig);
+module.exports = withNextra({});
 
 // If you have other Next.js configurations, you can pass them as the parameter:
 // module.exports = withNextra({ /* other next.js config */ })
