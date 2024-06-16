7bb7d6ce4a9c8da974bc9cf4bbe3fe62b45b1849
Michael-u21546551 <u21546551@tuks.co.za>
2024-06-16 20:52:02 +0200

    Update next.config.js

--- documentation/occupi-docs/next.config.js
+++ documentation/occupi-docs/next.config.js
@@ -1,9 +1,16 @@
 const withNextra = require('nextra')({
     theme: 'nextra-theme-docs',
     themeConfig: './theme.config.jsx'
-  })
+})
 
-module.exports = withNextra({});
+const isProd = process.env.NODE_ENV === 'production';
+
+const prodConfig = {
+  basePath: '/docs',
+  assetPrefix: '/docs/',
+};
+
+module.exports = withNextra(isProd ? prodConfig : {});
 
 // If you have other Next.js configurations, you can pass them as the parameter:
 // module.exports = withNextra({ /* other next.js config */ })
