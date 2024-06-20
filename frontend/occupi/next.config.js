49238a6f25ff36d876a2401b149eb8dc48d7337f
Michael <u21546551@tuks.co.za>
2024-06-20 22:48:20 +0200

    chore: Update Dockerfile and next.config.js for frontend/occupi

--- frontend/occupi/next.config.js
+++ frontend/occupi/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+// next.config.js
+module.exports = {
+  images: {
+    unoptimized: true,
+  },
+};
+
