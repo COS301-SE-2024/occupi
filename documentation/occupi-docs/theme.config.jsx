f51868e1c38fa92fdc9fbea3e27912e571a12360
Michael <u21546551@tuks.co.za>
2024-06-20 15:28:56 +0200

    feat: Remove unused files and update dependencies

--- documentation/occupi-docs/theme.config.jsx
+++ documentation/occupi-docs/theme.config.jsx
@@ -5,14 +5,14 @@ export default {
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <meta property="og:title" content="occupi" />
             <meta property="og:description" content="This is occupi-s documentation site" />
-            <link rel="icon" type="image/svg+xml" href="https://raw.githubusercontent.com/COS301-SE-2024/occupi/5f614e7d881c9d4f65ec2cf6ea60bf5542eb77a7/presentation/Occupi/image_2024-05-21_213821107.svg" />
+            <link rel="icon" type="image/svg+xml" href="https://raw.githubusercontent.com/COS301-SE-2024/occupi/5614db6d7821bb21b94125c83bc5a46126c5acac/frontend/occupi-web/public/occupi.svg" />
         </>
     ),
     logo: (
         <>
             <img 
                 style={{height: "30px"}}
-                src="https://raw.githubusercontent.com/COS301-SE-2024/occupi/5f614e7d881c9d4f65ec2cf6ea60bf5542eb77a7/presentation/Occupi/image_2024-05-21_213821107.svg" 
+                src="https://raw.githubusercontent.com/COS301-SE-2024/occupi/5614db6d7821bb21b94125c83bc5a46126c5acac/frontend/occupi-web/public/occupi.svg" 
                 />
             <span style={{ marginLeft: '.4em', fontWeight: 800 }}>
                 occupi-docs
---
+++ frontend/occupi/pages/index.js
@@ -0,0 +1,11 @@
+import Hero from "@/components/ui/Hero";
+import VisualFeatures from "@/components/ui/VisualFeatures";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <VisualFeatures />
+    </>
+  );
+}
