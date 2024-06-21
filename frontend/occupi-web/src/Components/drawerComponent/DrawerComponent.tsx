a3be0e11e49ecf0f92c41fdf5e06f4341ea4c848
Michael <u21546551@tuks.co.za>
2024-06-21 16:44:45 +0200

    Revert "Delete frontend/occupi-web/src/Components directory"
    This reverts commit 6ef87b40f4e52a534a39230b6827d76e87afd33b.

---
+++ frontend/occupi-web/src/Components/drawerComponent/DrawerComponent.tsx
@@ -0,0 +1,54 @@
+import { motion } from "framer-motion";
+import MenuItem from "./menuItem/MenuItem"; // Make sure to adjust the path according to your directory structure
+import {Userprofile, Pallete, Privacy, AlertIcon, HelpIcon} from "@assets/index";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { Appearance } from "@components/index";
+import { SettingsImg } from "@assets/index";
+
+const DrawerComponent = () => {
+  const navigate = useNavigate();
+  const [selectedItem, setSelectedItem] = useState<string>("/profile");
+  const [showAppearance, setShowAppearance] = useState(false);
+
+  const handleClick = (path: string) => {
+    setSelectedItem(path);
+    navigate("/settings" + path);
+  };
+
+  return (
+    <div className="flex flex-row relative">
+      <motion.div
+        className="w-full  z-10"
+        initial={{ x: "-1%" }}
+        animate={{ x: 20 }}
+        transition={{ duration: 0.2, ease: "linear" }}
+      >
+        <div className="menu p-4 w-80 bg-base-100 text-base-content bg-transparent">
+          <ul className="menu-items ">
+            <MenuItem icon={<Userprofile />} selectedItem={selectedItem} text="Profile" path="/profile" handleClick={handleClick} />
+            <MenuItem icon={<Pallete />} selectedItem={selectedItem} text="Appearance" path="/appearance" handleClick={handleClick} />
+          </ul>
+        </div>
+      </motion.div>
+      <div className=" absolute left-0  top-0">
+        {showAppearance ? (
+          <Appearance />
+        ) : (
+          <div className="flex flex-col items-center ml-96 mt-16 transition transform hover:scale-105 duration-500">
+            <span className=" font-serif font-semibold text-3xl">Please Select a Setting</span>
+            <img
+              src={SettingsImg}
+              alt="Settings"
+              className="object-scale-down  "
+              
+            />
+            
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default DrawerComponent;
