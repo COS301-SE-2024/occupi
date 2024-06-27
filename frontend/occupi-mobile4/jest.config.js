6f8e91448f8c8ba0aa5e3414f34aa5d3bac7a5d4
cmokou <klifo14@gmail.com>
2024-06-27 17:14:17 +0200

    chore: Add Jest configuration for mobile testing

---
+++ frontend/occupi-mobile4/jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  preset: 'jest-expo',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@gluestack-ui/.*)'
+  ],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^react-native$': require.resolve('react-native'),
+    '^react$': require.resolve('react')
+  },
+  testEnvironment: 'jsdom',
+  setupFiles: ['./jest.setup.js'],
+};
