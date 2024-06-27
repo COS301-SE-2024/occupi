46b6cd9e2e0807ce498c594a2dd4610c91aabb9f
cmokou <klifo14@gmail.com>
2024-06-27 21:39:43 +0200

    chore: Update Jest setup for mocking react-native-safe-area-context

--- frontend/occupi-mobile4/jest.setup.js
+++ frontend/occupi-mobile4/jest.setup.js
@@ -31,6 +31,11 @@ jest.mock('@expo/vector-icons', () => {
   };
 });
 
+jest.mock('react-native-safe-area-context', () => ({
+    SafeAreaProvider: ({ children }) => children,
+    useSafeAreaInsets: () => ({ top: 0, right: 0, bottom: 0, left: 0 }),
+  }));
+  
 jest.mock('expo-blur', () => {
   const React = require('react');
   const MockBlurView = (props) => {
