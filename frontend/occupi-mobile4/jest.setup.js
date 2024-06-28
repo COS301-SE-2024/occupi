3d55c22dfb23d535fa9506b64444c315fa0e7796
cmokou <klifo14@gmail.com>
2024-06-28 23:41:06 +0200

    chore: Update Jest setup for mocking react-native-gesture-handler, react-native-reanimated, and @gluestack-ui/themed

--- frontend/occupi-mobile4/jest.setup.js
+++ frontend/occupi-mobile4/jest.setup.js
@@ -1,5 +1,41 @@
 import 'react-native-gesture-handler/jestSetup';
 
+// Mocking react-native-gesture-handler
+jest.mock('react-native-gesture-handler', () => {
+  const View = require('react-native/Libraries/Components/View/View');
+  return {
+    Swipeable: View,
+    DrawerLayout: View,
+    State: {},
+    ScrollView: View,
+    Slider: View,
+    Switch: View,
+    TextInput: View,
+    ToolbarAndroid: View,
+    ViewPagerAndroid: View,
+    DrawerLayoutAndroid: View,
+    WebView: View,
+    NativeViewGestureHandler: View,
+    TapGestureHandler: View,
+    FlingGestureHandler: View,
+    ForceTouchGestureHandler: View,
+    LongPressGestureHandler: View,
+    PanGestureHandler: View,
+    PinchGestureHandler: View,
+    RotationGestureHandler: View,
+    /* Buttons */
+    RawButton: View,
+    BaseButton: View,
+    RectButton: View,
+    BorderlessButton: View,
+    /* Other */
+    FlatList: View,
+    gestureHandlerRootHOC: jest.fn(),
+    Directions: {},
+  };
+});
+
+
 jest.mock('react-native-reanimated', () => {
     const Reanimated = require('react-native-reanimated/mock');
     Reanimated.default.call = () => {};
@@ -75,3 +111,21 @@ jest.mock('@gluestack-ui/themed', () => ({
     };
     return RN;
   });
+
+
+jest.mock('@gluestack-ui/themed', () => ({
+  Image: 'Image',
+  Center: 'Center',
+  Text: 'Text',
+  Heading: 'Heading',
+}));
+
+jest.mock('react-native-responsive-screen', () => ({
+  widthPercentageToDP: jest.fn(),
+  heightPercentageToDP: jest.fn(),
+}));
+
+jest.mock('@react-native-async-storage/async-storage', () => require('@react-native-async-storage/async-storage/jest/async-storage-mock'));
+jest.mock('expo-linear-gradient', () => ({
+  LinearGradient: 'LinearGradient',
+}));
