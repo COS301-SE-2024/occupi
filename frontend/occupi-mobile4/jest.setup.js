84b741efac41a135973cb95a2cb7604161919a0a
cmokou <klifo14@gmail.com>
2024-06-29 18:20:20 +0200

    chore: now handles animated images

--- frontend/occupi-mobile4/jest.setup.js
+++ frontend/occupi-mobile4/jest.setup.js
@@ -1,115 +1,15 @@
+global.setImmediate = global.setImmediate || ((fn, ...args) => global.setTimeout(fn, 0, ...args));
 import 'react-native-gesture-handler/jestSetup';
 
-jest.mock('@react-navigation/stack', () => {
-  return {
-    createStackNavigator: jest.fn(() => ({
-      Navigator: 'Navigator',
-      Screen: 'Screen',
-    })),
-  };
-});
+jest.useFakeTimers();
 
-jest.mock('@react-navigation/native', () => {
-  return {
-    ...jest.requireActual('@react-navigation/native'),
-    useNavigation: () => ({
-      navigate: jest.fn(),
-      goBack: jest.fn(),
-    }),
-  };
-});
+it('should render SplashScreen and navigate after timeout', () => {
+  const tree = renderer.create(<SplashScreen />);
+  expect(tree).toBeTruthy();
 
-jest.mock('react-native', () => {
-  const RN = jest.requireActual('react-native');
-  return {
-    ...RN,
-    NativeModules: {
-      ...RN.NativeModules,
-      SettingsManager: {
-        settings: { AppleLocale: 'en_US' },
-        get: jest.fn(),
-        set: jest.fn(),
-      },
-      StatusBarManager: {
-        getHeight: jest.fn(),
-      },
-    },
-    StyleSheet: {
-      ...RN.StyleSheet,
-      create: (styles) => styles,
-      hairlineWidth: 1,
-    },
-    Dimensions: {
-      get: jest.fn().mockImplementation((dim) => {
-        switch (dim) {
-          case 'window':
-            return { width: 360, height: 640, scale: 2, fontScale: 2 };
-          case 'screen':
-            return { width: 360, height: 640, scale: 2, fontScale: 2 };
-          default:
-            return { width: 360, height: 640, scale: 2, fontScale: 2 };
-        }
-      }),
-      set: jest.fn(),
-      addEventListener: jest.fn(),
-      removeEventListener: jest.fn(),
-      screen: {
-        width: 360,
-        height: 640,
-        scale: 2,
-        fontScale: 2,
-      },
-      window: {
-        width: 360,
-        height: 640,
-        scale: 2,
-        fontScale: 2,
-      },
-    },
-    PixelRatio: {
-      get: jest.fn(() => 2),
-      getFontScale: jest.fn(() => 2),
-      getPixelSizeForLayoutSize: jest.fn((size) => size * 2),
-      roundToNearestPixel: jest.fn((size) => Math.round(size)),
-    },
-    NativeEventEmitter: jest.fn().mockImplementation(() => ({
-      addListener: jest.fn(),
-      removeListeners: jest.fn(),
-    })),
-  };
-});
+  jest.runAllTimers();
 
-jest.mock('react-native-gesture-handler', () => {
-  const View = require('react-native/Libraries/Components/View/View');
-  return {
-    GestureHandlerRootView: View,
-    Swipeable: View,
-    DrawerLayout: View,
-    State: {},
-    ScrollView: View,
-    Slider: View,
-    Switch: View,
-    TextInput: View,
-    ToolbarAndroid: View,
-    ViewPagerAndroid: View,
-    DrawerLayoutAndroid: View,
-    WebView: View,
-    NativeViewGestureHandler: View,
-    TapGestureHandler: View,
-    FlingGestureHandler: View,
-    ForceTouchGestureHandler: View,
-    LongPressGestureHandler: View,
-    PanGestureHandler: View,
-    PinchGestureHandler: View,
-    RotationGestureHandler: View,
-    RawButton: View,
-    BaseButton: View,
-    RectButton: View,
-    BorderlessButton: View,
-    FlatList: View,
-    gestureHandlerRootHOC: jest.fn(),
-    Directions: {},
-  };
+  // Add assertions for navigation here
 });
 
 jest.mock('react-native-reanimated', () => {
@@ -120,118 +20,33 @@ jest.mock('react-native-reanimated', () => {
 
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 
-jest.mock('@expo/vector-icons', () => {
+jest.mock('expo-linear-gradient', () => {
   const React = require('react');
-  const PropTypes = require('prop-types');
-
-  const MockIcon = (props) => {
-    return React.createElement('svg', {
-      ...props,
-      children: props.children || 'icon',
-    });
-  };
-
-  MockIcon.propTypes = {
-    name: PropTypes.string,
-  };
-
+  const { View } = require('react-native');
   return {
-    Ionicons: MockIcon,
-    Octicons: MockIcon,
-    Feather: MockIcon,
-    FontAwesome6: MockIcon,
+    LinearGradient: ({ children, ...props }) => {
+      return React.createElement(View, props, children);
+    },
   };
 });
 
-jest.mock('react-native-safe-area-context', () => ({
-  SafeAreaProvider: ({ children }) => children,
-  useSafeAreaInsets: () => ({ top: 0, right: 0, bottom: 0, left: 0 }),
+jest.mock('expo-router', () => ({
+  router: {
+    replace: jest.fn(),
+  },
 }));
-
-jest.mock('expo-blur', () => {
-  const React = require('react');
-  const MockBlurView = (props) => {
-    return React.createElement('view', props, props.children);
-  };
-  return {
-    BlurView: MockBlurView,
-  };
-});
 
 jest.mock('react-native-responsive-screen', () => ({
   widthPercentageToDP: jest.fn(),
   heightPercentageToDP: jest.fn(),
 }));
 
-jest.mock('expo-router', () => ({
-  useRouter: () => ({
-    push: jest.fn(),
-  }),
+jest.mock('@ui-kitten/components', () => ({
+  ViewPager: 'ViewPager',
 }));
-
-jest.mock('@gluestack-ui/themed', () => ({
-  ...jest.requireActual('@gluestack-ui/themed'),
-  useToast: () => ({
-    show: jest.fn(),
-  }),
-}));
-
-jest.mock('@react-native-async-storage/async-storage', () => require('@react-native-async-storage/async-storage/jest/async-storage-mock'));
-
-jest.mock('expo-linear-gradient', () => ({
-  LinearGradient: 'LinearGradient',
-}));
-
-jest.mock('react-native/Libraries/Settings/Settings', () => ({
-  get: jest.fn(),
-  set: jest.fn(),
-}));
-
-jest.mock('react-native/Libraries/TurboModule/TurboModuleRegistry', () => ({
-  get: jest.fn(() => ({
-    getConstants: () => ({}),
-    get: jest.fn(),
-    set: jest.fn(),
-  })),
-  getEnforcing: jest.fn(() => ({
-    getConstants: () => ({}),
-    get: jest.fn(),
-    set: jest.fn(),
-  })),
-}));
-
-jest.mock('react-native/Libraries/Utilities/Dimensions', () => ({
-  get: jest.fn().mockReturnValue({
-    width: 375,
-    height: 667,
-    scale: 1,
-    fontScale: 1,
-  }),
-  set: jest.fn(),
-  addEventListener: jest.fn(),
-  removeEventListener: jest.fn(),
-  screen: {
-    width: 375,
-    height: 667,
-    scale: 1,
-    fontScale: 1,
-  },
-}));
-
-jest.mock('react-native/Libraries/Utilities/PixelRatio', () => ({
-  get: jest.fn(() => 1),
-  getFontScale: jest.fn(() => 1),
-  getPixelSizeForLayoutSize: jest.fn((size) => size),
-  roundToNearestPixel: jest.fn((size) => size),
-  startDetecting: jest.fn(),
-}));
-
-jest.mock('react-native/Libraries/StyleSheet/StyleSheet', () => ({
-  create: jest.fn((styles) => styles),
-  hairlineWidth: 1,
-  absoluteFill: {},
-  absoluteFillObject: {},
-  flatten: jest.fn(),
-  compose: jest.fn(),
-  roundToNearestPixel: jest.fn((value) => value),
-}));
+  
+  jest.mock('react-native-reanimated', () => {
+    const Reanimated = require('react-native-reanimated/mock');
+    Reanimated.default.call = () => {};
+    return Reanimated;
+  });
