3fa4fe02928dfd75a42730fb8bb652944135ebe9
cmokou <klifo14@gmail.com>
2024-06-29 18:19:53 +0200

    chore: Update Jest setup to handle image files in tests

--- frontend/occupi-mobile4/jest.config.js
+++ frontend/occupi-mobile4/jest.config.js
@@ -1,17 +1,24 @@
 module.exports = {
   preset: 'react-native',
-  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  setupFilesAfterEnv: ['./jest.setup.js',
+    'react-native-gesture-handler/jestSetup'],
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@gluestack-ui/.*)'
+    'node_modules/(?!(@react-native|react-native|@react-native-community|@expo|expo|@expo-google-fonts|@unimodules|unimodules|sentry-expo|native-base|react-native-svg|@gluestack-ui|@ui-kitten)/)',
   ],
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+    '^.+\\.(png|jpg|jpeg|svg)$': 'jest-transform-stub',  // Add this line to handle images
   },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
     '^react-native$': require.resolve('react-native'),
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
     '^react$': require.resolve('react')
   },
   testEnvironment: 'jsdom',
-  setupFiles: ['./jest.setup.js'],
+  testMatch: [
+    '**/__tests__/**/*.js?(x)',
+    '**/?(*.)(spec|test).js?(x)'
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
