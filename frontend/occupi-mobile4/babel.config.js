580c1daee3e1d5b87d5268cc74539cf76798cb2c
cmokou <klifo14@gmail.com>
2024-06-29 18:32:32 +0200

    chore: Update babel.config.js presets and plugins

--- frontend/occupi-mobile4/babel.config.js
+++ frontend/occupi-mobile4/babel.config.js
@@ -1,7 +1,14 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ['@babel/plugin-proposal-export-namespace-from'],
+    presets: [
+      'babel-preset-expo',
+      '@babel/preset-react',
+      '@babel/preset-typescript',
+    ],
+    plugins: [
+      '@babel/plugin-proposal-export-namespace-from',
+      '@babel/plugin-transform-react-jsx',
+    ],
   };
 };
