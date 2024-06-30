95fc085aedb762ad8eab86284fdf1645c3ea157b
Michael <u21546551@tuks.co.za>
2024-06-30 18:05:24 +0200

    chore: Add function to send bulk emails with BCC

--- occupi-backend/pkg/mail/mail.go
+++ occupi-backend/pkg/mail/mail.go
@@ -1,10 +1,20 @@
 package mail
 
 import (
+	"errors"
+	"strings"
 	"sync"
+	"time"
 
 	"github.com/COS301-SE-2024/occupi/occupi-backend/configs"
 	"gopkg.in/gomail.v2"
+)
+
+var (
+	mu                sync.Mutex
+	emailsSent        int
+	recipientsReached int
+	dayStart          time.Time
 )
 
 // SendMail sends an email using gomail
@@ -51,4 +61,62 @@ func SendMultipleEmailsConcurrently(emails []string, subject, body string) []str
 	wg.Wait()
 
 	return emailErrors
+}
+
+func SendBulkEmailWithBCC(emails []string, subject, body string) error {
+	// Lock to prevent
+	mu.Lock()
+
+	// if new day, reset email count
+	if time.Since(dayStart) > 24*time.Hour {
+		dayStart = time.Now()
+		emailsSent = 0
+		recipientsReached = 0
+	}
+
+	// if emails exceed 10, return error
+	if len(emails) > 10 {
+		return errors.New("exceeded maximum number of recipients")
+	}
+
+	// if we will exceed max allowed emails of 50 per day, return error
+	if emailsSent+len(emails) > 50 {
+		return errors.New("exceeded maximum number of emails sent per day")
+	}
+
+	// if we will exceed max allowed recipients of 500 per day, return error
+	if recipientsReached+len(emails) > 500 {
+		return errors.New("exceeded maximum number of recipients per day")
+	}
+
+	// Send the email
+	if configs.GetGinRunMode() != "test" {
+		bcc := strings.Join(emails, ",")
+		if err := SendMailBCC(subject, body, bcc); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func SendMailBCC(subject, body, bcc string) error {
+	from := configs.GetSystemEmail()
+	password := configs.GetSMTPPassword()
+	smtpHost := configs.GetSMTPHost()
+	smtpPort := configs.GetSMTPPort()
+
+	m := gomail.NewMessage()
+	m.SetHeader("From", from)
+	m.SetHeader("Subject", subject)
+	m.SetBody("text/html", body)
+	m.SetHeader("Bcc", bcc)
+
+	d := gomail.NewDialer(smtpHost, smtpPort, from, password)
+
+	if err := d.DialAndSend(m); err != nil {
+		return err
+	}
+
+	return nil
 }
