799c40c48a554c90d7b76ecc94603f037f605ad8
cmokou <klifo14@gmail.com>
2024-07-01 10:18:47 +0200

    chore: Update GradientButton component to include testID prop

--- frontend/occupi-mobile4/screens/Login/Onboarding2.tsx
+++ frontend/occupi-mobile4/screens/Login/Onboarding2.tsx
@@ -10,19 +10,20 @@ import { StyleSheet, View } from 'react-native';
 import { router } from 'expo-router';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
-const GradientButton = ({ onPress, text }) => (
+const GradientButton = ({ onPress, text, testID }) => (
   <LinearGradient
-    colors={['#614DC8', '#86EBCC', '#B2FC3A', '#EEF060']}
+    colors={["#614DC8", "#86EBCC", "#B2FC3A", "#EEF060"]}
     locations={[0.02, 0.31, 0.67, 0.97]}
     start={[0, 1]}
     end={[1, 0]}
     style={styles.buttonContainer}
   >
-    <Heading style={styles.buttonText} onPress={onPress}>
+    <Heading style={styles.buttonText} onPress={onPress} testID={testID}>
       {text}
     </Heading>
   </LinearGradient>
 );
+
 
 const Onboarding2 = () => {
   return (
