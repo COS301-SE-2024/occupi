5798954ef3c5f190d39318862f1af5afbb9419e5
Kamogelo Moeketse <kamogelomoeketse@gmail.com>
2024-07-02 19:45:35 +0200

    componenent directories fix

--- frontend/occupi-web/src/pages/index.ts
+++ frontend/occupi-web/src/pages/index.ts
@@ -2,7 +2,7 @@ import LoginForm from './Login/LoginForm'
 import OtpPage from './otp-page/OtpPage'
 import LandingPage from './landing-page/LandingPage'
 import Settings from './settings-page/Settings'
-import Dashboard from "./dashboard/Dashboard";
+import Dashboard from "./Dashboard/Dashboard";
 import Appearance from './appearance-page/Appearance';
 import OverView from '../components/bookings/Bookings';
 import Analysis from './analysis/Analysis';
