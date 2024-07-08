90deff4cc01b6b36c5548f753652177de9dd1d12
cmokou <klifo14@gmail.com>
2024-07-08 08:47:32 +0200

    refactor: Remove GetFrontendURL function and add GetResetOTP function to retrieve OTP from the database

--- occupi-backend/configs/config.go
+++ occupi-backend/configs/config.go
@@ -242,11 +242,3 @@ func GetOTPExpiration() int {
 	return expiration
 }
 
-// gets the frontend url as defined in the config.yaml file in seconds
-func GetFrontendURL() string {
-	frontendURL := viper.GetString("FRONTEND_URL")
-	if frontendURL == "" {
-		frontendURL = "FRONTEND_URL"
-	}
-	return frontendURL
-}
