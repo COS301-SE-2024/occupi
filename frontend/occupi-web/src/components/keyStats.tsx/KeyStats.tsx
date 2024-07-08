52df708f6b1d8364ab2a786b25a1ac1a54ffca3a
Tinashe-Austin <tinashe.austin@tuks.co.za>
2024-07-08 19:29:33 +0200

    feat: Add KeyStats component for displaying office occupancy statistics

---
+++ frontend/occupi-web/src/components/keyStats.tsx/KeyStats.tsx
@@ -0,0 +1,12 @@
+export default function KeyStats() {
+    return (
+      <div className="text-text_col flex flex-col justify-center items-center">
+        <h3 className="text-lg font-semibold mb-2">Key Stats</h3>
+        <div className="text-text_col">
+          <p className=" text-text_col">Average Weekly Hours: 7.6</p>
+          <p className=" text-text_col">Most Likely Office Time: 11AM</p>
+          <p className=" text-text_col">Occupancy Rating: 70%</p>
+        </div>
+      </div>
+    );
+  }
