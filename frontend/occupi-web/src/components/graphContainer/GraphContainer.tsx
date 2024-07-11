247017835a2aef33273dbb7a1fd8189a111979a4
Tinashe-Austin <tinashe.austin@tuks.co.za>
2024-07-11 23:35:16 +0200

    Update GraphContainer component styling and layout

--- frontend/occupi-web/src/components/graphContainer/GraphContainer.tsx
+++ frontend/occupi-web/src/components/graphContainer/GraphContainer.tsx
@@ -8,8 +8,8 @@ interface GraphContainerProps {
 
 const GraphContainer: React.FC<GraphContainerProps> = ({ width = '24.531vw', height = '13.49vw' ,mainComponent}) => {
   return (
-    <div data-testid='graph-container' className=''>
-      <div style={{ width, height }} data-testid="graph-container" className={`card border-2 border-tertiary rounded-[20px] bg-secondary w-[${width}] h-[${height}] shadow-2xl rounded-[20px]`} >      {mainComponent}
+    <div data-testid='graph-container' className='mb-5'>
+      <div style={{ width, height }} data-testid="graph-container" className={`card border-2 border-tertiary rounded-[20px] bg-secondary`} >      {mainComponent}
       </div>
     </div>
   );
