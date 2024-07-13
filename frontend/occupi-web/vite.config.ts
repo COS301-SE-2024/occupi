90aa04027a2269cf48e856c60d9ad5baa4fd1b78
Michael-u21546551 <u21546551@tuks.co.za>
2024-07-13 19:59:49 +0200

    chore: Update Dockerfile to use npm instead of bun for package installation

--- frontend/occupi-web/vite.config.ts
+++ frontend/occupi-web/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import tsconfigPaths from "vite-tsconfig-paths";
 import mkcert from "vite-plugin-mkcert";
+import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,6 +22,9 @@ export default defineConfig({
       usePolling: true,
       interval: 1000, // Adjust the interval if needed
     },
-    https: true, // Not needed for Vite 5+
+    https: {
+      key: fs.readFileSync('/etc/letsencrypt/live/dev.occupi.tech/privkey.pem'),
+      cert: fs.readFileSync('/etc/letsencrypt/live/dev.occupi.tech/fullchain.pem')
+    }
   },
 });
