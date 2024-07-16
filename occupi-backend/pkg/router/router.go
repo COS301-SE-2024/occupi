826bea6af5dd0e285a04977838ccbb060988f642
Rethakgetse-Manaka <rethakgetse11@gmail.com>
2024-07-16 16:48:53 +0200

    "Get users endpoint protected"

--- occupi-backend/pkg/router/router.go
+++ occupi-backend/pkg/router/router.go
@@ -63,7 +63,7 @@ func OccupiRouter(router *gin.Engine, db *mongo.Client, cache *bigcache.BigCache
 		api.GET("/user-details", middleware.ProtectedRoute, func(ctx *gin.Context) { handlers.GetUserDetails(ctx, appsession) })
 		api.PUT("/update-user", middleware.ProtectedRoute, func(ctx *gin.Context) { handlers.UpdateUserDetails(ctx, appsession) })
 		api.GET("/filter-users", middleware.ProtectedRoute, func(ctx *gin.Context) { handlers.FilterUsers(ctx, appsession) })
-		api.GET("/get-users", middleware.UnProtectedRoute, func(ctx *gin.Context) { handlers.GetUsers(ctx, appsession) })
+		api.GET("/get-users", middleware.ProtectedRoute, func(ctx *gin.Context) { handlers.GetUsers(ctx, appsession) })
 	}
 	auth := router.Group("/auth")
 	{
