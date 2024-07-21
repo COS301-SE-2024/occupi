ab7e9b75d2b6cc10553c9d7ab0dcaab703c2d6d2
Michael-u21546551 <u21546551@tuks.co.za>
2024-07-21 12:09:47 +0200

    chore: Update Dockerfile and docker-compose files for local development and fix compose files and vite config files for web

--- frontend/occupi-web/vite.config.ts
+++ frontend/occupi-web/vite.config.ts
@@ -3,15 +3,13 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import tsconfigPaths from "vite-tsconfig-paths";
 import fs from 'fs';
+import mkcert from 'vite-plugin-mkcert';
 
 // Define paths to the certificates
 let keyPath = '';
 let certPath = '';
 
-if (process.env.NODE_ENV === 'development') {
-  keyPath = path.resolve(__dirname, 'cert.key');
-  certPath = path.resolve(__dirname, 'cert.crt');
-} else if (process.env.NODE_ENV === 'preview-dev') {
+if (process.env.NODE_ENV === 'preview-dev') {
   keyPath = '/etc/letsencrypt/live/dev.occupi.tech/privkey.pem';
   certPath = '/etc/letsencrypt/live/dev.occupi.tech/fullchain.pem';
 } else if (process.env.NODE_ENV === 'preview-prod') {
@@ -21,7 +19,9 @@ if (process.env.NODE_ENV === 'development') {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), mkcert({
+    hosts: ['localhost', 'dev.occupi.tech', 'app.occupi.tech'],
+  })],
   
 
   resolve: {
@@ -36,9 +36,11 @@ export default defineConfig({
       usePolling: true,
       interval: 1000, // Adjust the interval if needed
     },
-    https: process.env.NODE_ENV === 'production' ? {} : {
+    https: process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'preview' || process.env.NODE_ENV === 'development' ? {} : {
       key: fs.readFileSync(keyPath),
       cert: fs.readFileSync(certPath),
     },
+    host: '0.0.0.0',
+    port: 4173,
   },
 });
