363cf2fe581aff2bec8164fe1fbfa6dfa244f81f
Kamogelo Moeketse <kamogelomoeketse@gmail.com>
2024-07-21 18:04:29 +0200

    code refactoring

--- frontend/occupi-mobile4/screens/Settings/Security.tsx
+++ frontend/occupi-mobile4/screens/Settings/Security.tsx
@@ -2,47 +2,21 @@ import React, { useState, useEffect } from 'react';
 import {
   TouchableOpacity,
   StyleSheet,
-  ScrollView,
   Alert,
-  Keyboard,
-  KeyboardAvoidingView,
-  Platform
 } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import { Feather } from '@expo/vector-icons';
 import { FontAwesome5 } from '@expo/vector-icons';
+import * as LocalAuthentication from "expo-local-authentication";
 import {
   Icon,
   View,
-  Text,
-  Input,
-  FormControl,
-  FormControlError,
-  FormControlErrorIcon,
-  FormControlErrorText,
-  FormControlLabel,
-  FormControlLabelText,
-  InputField,
-  InputIcon,
-  InputSlot
+  Text
 } from '@gluestack-ui/themed';
-import { Controller, useForm } from 'react-hook-form';
 import { router } from 'expo-router';
-import { AlertTriangle, EyeIcon, EyeOffIcon } from 'lucide-react-native';
 import { useColorScheme, Switch } from 'react-native';
-import { z } from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
 import GradientButton from '@/components/GradientButton';
 import * as SecureStore from 'expo-secure-store';
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import axios from 'axios';
-
-const COLORS = {
-  white: '#FFFFFF',
-  black: '#000000',
-  gray: '#BEBEBE',
-  primary: '#3366FF',
-};
 
 const FONTS = {
   h3: { fontSize: 20, fontWeight: 'bold' },
@@ -96,41 +70,68 @@ const Security = () => {
     setNewForceLogout(previousState => !previousState);
   };
 
+  const handleBiometricAuth = async () => {
+    const hasHardware = await LocalAuthentication.hasHardwareAsync();
+    const isEnrolled = await LocalAuthentication.isEnrolledAsync();
+
+    if (!hasHardware || !isEnrolled) {
+      Alert.alert(
+        "Biometric Authentication not available",
+        "Your device does not support biometric authentication or it is not set up. Please use your PIN to confirm the booking."
+      );
+      return;
+    }
+
+    const result = await LocalAuthentication.authenticateAsync({
+      promptMessage: "Confirm your booking",
+      fallbackLabel: "Use PIN",
+    });
+
+    if (result.success) {
+      router.push('/changepassword');
+    } else {
+      Alert.alert(
+        "Authentication failed",
+        "Biometric authentication failed. Please try again."
+      );
+    }
+  };
+
   const onSave = async () => {
     //integration here
-      let userEmail = await SecureStore.getItemAsync('Email');
-      let authToken = await SecureStore.getItemAsync('Token');
+    let userEmail = await SecureStore.getItemAsync('Email');
+    let authToken = await SecureStore.getItemAsync('Token');
 
-      try {
-        const response = await axios.post('https://dev.occupi.tech/api/update-security-settings', {
-          email: userEmail,
+    try {
+      const response = await axios.post('https://dev.occupi.tech/api/update-security-settings', {
+        email: userEmail,
+        mfa: newMfa ? "on" : "off",
+        forceLogout: newForceLogout ? "on" : "off"
+      }, {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': `${authToken}`
+        },
+        withCredentials: true
+      });
+      const data = response.data;
+      // console.log(`Response Data: ${JSON.stringify(data.data)}`);
+      console.log(data);
+      if (response.status === 200) {
+        const newSettings = {
           mfa: newMfa ? "on" : "off",
-          forceLogout: newForceLogout ? "on" : "off"
-        }, {
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': `${authToken}`
-          },
-          withCredentials: true
-        });
-        const data = response.data;
-        // console.log(`Response Data: ${JSON.stringify(data.data)}`);
-        console.log(data);
-        if (response.status === 200) {
-          const newSettings = {
-            mfa: newMfa ? "on" : "off",
-            forceLogout: newForceLogout ? "on" : "off",
-          }
-          console.log(newSettings);
-          SecureStore.setItemAsync('Security', JSON.stringify(newSettings));
-          router.replace('/settings');
-        } else {
-          console.log(data);
+          forceLogout: newForceLogout ? "on" : "off",
         }
-      } catch (error) {
-        console.error('Error:', error);
+        console.log(newSettings);
+        SecureStore.setItemAsync('Security', JSON.stringify(newSettings));
+        router.replace('/settings');
+      } else {
+        console.log(data);
       }
+    } catch (error) {
+      console.error('Error:', error);
+    }
   };
 
   const handleBack = () => {
@@ -157,55 +158,55 @@ const Security = () => {
 
 
     <View flex={1} backgroundColor={colorScheme === 'dark' ? 'black' : 'white'} px="$4" pt="$16">
-      <KeyboardAvoidingView
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-        style={{ flex: 1 }}
-      >
-        <View flex={1}>
-          <View style={styles.header}>
-            <Icon
-              as={Feather}
-              name="chevron-left"
-              size="xl"
-              color={colorScheme === 'dark' ? 'white' : 'black'}
-              onPress={handleBack}
-            />
-            <Text style={styles.headerTitle} color={colorScheme === 'dark' ? 'white' : 'black'}>
-              Security
-            </Text>
-            <FontAwesome5
-              name="fingerprint"
-              size={24}
-              color={colorScheme === 'dark' ? 'white' : 'black'}
-              style={styles.icon}
-            />
-          </View>
-
-
-          <View flexDirection="column">
-            <View my="$2" h="$12" justifyContent="space-between" alignItems="center" flexDirection="row" px="$3" borderRadius={14} backgroundColor={colorScheme === 'dark' ? '#2C2C2E' : '#F3F3F3'}>
-              <Text color={colorScheme === 'dark' ? 'white' : 'black'}>Use 2fa to login</Text>
-              <Switch
-                trackColor={{ false: 'lightgray', true: 'lightgray' }}
-                thumbColor={newMfa ? 'greenyellow' : 'white'}
-                ios_backgroundColor="lightgray"
-                onValueChange={toggleSwitch1}
-                value={newMfa}
-              />
-            </View>
-            <View my="$2" h="$12" justifyContent="space-between" alignItems="center" flexDirection="row" px="$3" borderRadius={14} backgroundColor={colorScheme === 'dark' ? '#2C2C2E' : '#F3F3F3'}>
-              <Text color={colorScheme === 'dark' ? 'white' : 'black'}>Force logout on app close</Text>
-              <Switch
-                trackColor={{ false: 'lightgray', true: 'lightgray' }}
-                thumbColor={newForceLogout ? 'greenyellow' : 'white'}
-                ios_backgroundColor="lightgray"
-                onValueChange={toggleSwitch2}
-                value={newForceLogout}
-              />
-            </View>
-          </View>
+      <View flex={1}>
+        <View style={styles.header}>
+          <Icon
+            as={Feather}
+            name="chevron-left"
+            size="xl"
+            color={colorScheme === 'dark' ? 'white' : 'black'}
+            onPress={handleBack}
+          />
+          <Text style={styles.headerTitle} color={colorScheme === 'dark' ? 'white' : 'black'}>
+            Security
+          </Text>
+          <FontAwesome5
+            name="fingerprint"
+            size={24}
+            color={colorScheme === 'dark' ? 'white' : 'black'}
+            style={styles.icon}
+          />
         </View>
-      </KeyboardAvoidingView>
+
+
+        <View flexDirection="column">
+          <View my="$2" h="$12" justifyContent="space-between" alignItems="center" flexDirection="row" px="$3" borderRadius={14} backgroundColor={colorScheme === 'dark' ? '#2C2C2E' : '#F3F3F3'}>
+            <Text color={colorScheme === 'dark' ? 'white' : 'black'}>Use 2fa to login</Text>
+            <Switch
+              trackColor={{ false: 'lightgray', true: 'lightgray' }}
+              thumbColor={newMfa ? 'greenyellow' : 'white'}
+              ios_backgroundColor="lightgray"
+              onValueChange={toggleSwitch1}
+              value={newMfa}
+            />
+          </View>
+          <View my="$2" h="$12" justifyContent="space-between" alignItems="center" flexDirection="row" px="$3" borderRadius={14} backgroundColor={colorScheme === 'dark' ? '#2C2C2E' : '#F3F3F3'}>
+            <Text color={colorScheme === 'dark' ? 'white' : 'black'}>Force logout on app close</Text>
+            <Switch
+              trackColor={{ false: 'lightgray', true: 'lightgray' }}
+              thumbColor={newForceLogout ? 'greenyellow' : 'white'}
+              ios_backgroundColor="lightgray"
+              onValueChange={toggleSwitch2}
+              value={newForceLogout}
+            />
+          </View>
+          <TouchableOpacity onPress={() => handleBiometricAuth()}>
+            <View flexDirection="row" my="$2" borderRadius={14} alignItems="center" justifyContent="center" backgroundColor={colorScheme === 'dark' ? '#2C2C2E' : '#F3F3F3'} h="$12">
+              <Text fontWeight="bold" color={colorScheme === 'dark' ? '#fff' : '#000'}>Change Password</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </View>
       <View position="absolute" left={0} right={0} bottom={36}>
         <GradientButton
           onPress={onSave}
