504d711bb1cc5fa6f75beb6bfa78c8682f8a7fd3
Kamogelo Moeketse <kamogelomoeketse@gmail.com>
2024-07-23 22:49:16 +0200

    added rooms and booking models

--- frontend/occupi-mobile4/models/requests.ts
+++ frontend/occupi-mobile4/models/requests.ts
@@ -24,3 +24,58 @@ export interface ResetPassword {
     newPasswordConfirm: string;
     otp: string;
 }
+
+//API models
+
+//Rooms & Bookings
+
+export interface BookRoom {
+    roomId: string;
+    emails: string[];
+    creator: string;
+    floorNo: string; //string integer
+    date: string;
+    start: string;
+    end: string;
+}
+
+export interface ViewBookings {
+    operator: string;
+    filter: {
+        email: string;
+        date?: string;
+    };
+    order_asc?: string;
+    order_desc?: string;
+    projection?: string[];
+    limit?: number;
+    page?: number;
+}
+
+export interface ViewRooms {
+    operator: string;
+    filter: {
+        floorNo: string;
+    };
+    order_asc?: string;
+    order_desc?: string;
+    projection?: string[];
+    limit?: number;
+    page?: number;
+}
+
+export interface CancelBooking {
+    bookingId: string;
+    roomId: string;
+    emails: string[];
+    creator: string;
+    floorNo: string; //string integer
+    date: string;
+    start: string;
+    end: string;
+}
+
+export interface CheckIn {
+    bookingId: string;
+    email: string;
+}
