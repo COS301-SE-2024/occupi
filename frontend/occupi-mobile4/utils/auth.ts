5772fc29ddaad9ea000b14e30b1a67af011971e6
Kamogelo Moeketse <kamogelomoeketse@gmail.com>
2024-07-23 22:25:16 +0200

    auth models complete

--- frontend/occupi-mobile4/utils/auth.ts
+++ frontend/occupi-mobile4/utils/auth.ts
@@ -1,2 +1,2 @@
 //this folder contains functions that will call the service functions which make api requests for authentication
-//the purpose of this file is to refine and process the data and return these
+//the purpose of this file is to refine and process the data and return these to the ViewModel
