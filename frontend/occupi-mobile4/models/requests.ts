82e3fa6376a4b39d01cc4bc1f98a72d05c59a46f
Kamogelo Moeketse <kamogelomoeketse@gmail.com>
2024-07-23 22:25:16 +0200

    auth models complete

--- frontend/occupi-mobile4/models/requests.ts
+++ frontend/occupi-mobile4/models/requests.ts
@@ -1,1 +1,26 @@
 //these models describe data to be sent to the api (POST body)
+
+//Auth Requests
+
+export interface Login {
+	email: string;
+	password: string;
+}
+
+export interface Register {
+    email: string;
+    password: string;
+    employee_id: string;
+}
+
+export interface VerifyOTP {
+    email: string;
+    otp: string;
+}
+
+export interface ResetPassword {
+    email: string;
+    newPassword: string;
+    newPasswordConfirm: string;
+    otp: string;
+}
