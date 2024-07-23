6f7a4684c506527adaa016c567a9738e0900d4e0
Kamogelo Moeketse <kamogelomoeketse@gmail.com>
2024-07-23 22:10:00 +0200

    creating file structure for code refactoring

---
+++ frontend/occupi-mobile4/utils/auth.ts
@@ -0,0 +1,2 @@
+//this folder contains functions that will call the service functions which make api requests for authentication
+//the purpose of this file is to refine and process the data and return these
