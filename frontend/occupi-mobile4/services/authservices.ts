7ba4f7f708e690c705b750d8968b1427465a3ca9
Kamogelo Moeketse <kamogelomoeketse@gmail.com>
2024-07-24 14:18:14 +0200

    login service and utils function

---
+++ frontend/occupi-mobile4/services/authservices.ts
@@ -0,0 +1,32 @@
+import { LoginReq } from "@/models/requests";
+import { Error, LoginSuccess } from "@/models/response";
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+// dotenv.config();
+// const devUrl = process.env.EXPO_PUBLIC_DEVELOP_API_URL;
+// console.log(devUrl);
+
+export async function login(req: LoginReq): Promise<LoginSuccess | Error> {
+    try {
+        const response = await axios.post("https://dev.occupi.tech/auth/login-mobile", req, {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            withCredentials: true
+        });
+        return response.data;
+    } catch (error) {
+        if (axios.isAxiosError(error) && error.response) {
+            return error.response.data;
+        } else {
+            throw error;
+        }
+    }
+}
+
+// login({
+//     email: "boygenius31115@gmail.com",
+//     password: "Qwert@123"
+// })
