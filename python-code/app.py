c5e4f9216b78ab3a2b0294438e4e57c86016cf0b
Rethakgetse-Manaka <rethakgetse11@gmail.com>
2024-07-26 12:23:32 +0200

    Using flask for python api endpoints

--- python-code/app.py
+++ python-code/app.py
@@ -1,97 +1,94 @@
+# app.py
 from flask import Flask, request, jsonify
-import numpy as np
-import requests
-import json
-from sklearn.preprocessing import StandardScaler
 import logging
-
-# Define the URL for the TensorFlow Serving API
-url = 'http://localhost:8501/v1/models/attendance_model:predict'
-
-# Define the attendance levels based on the bin ranges
-attendance_levels = ["0-300", "300-600", "600-900", "900-1200", "1200-1500", "1500-1800", "1800+"]
-
-# Function to interpret the predictions
-def interpret_predictions(predictions):
-    # Get the predicted class index (the one with the highest probability)
-    predicted_class = int(np.argmax(predictions[0]))  # Convert to native Python int
-    
-    # Map the predicted class index to the attendance level
-    predicted_attendance_level = attendance_levels[predicted_class]
-    
-    return predicted_class, predicted_attendance_level
-
-# Function to send a prediction request
-def get_prediction(day_of_week, month, day_of_month, is_weekend, special_event, scaler):
-    try:
-        # Create sample input data based on your model's input shape
-        sample_input = np.array([[day_of_week, month, day_of_month, is_weekend, special_event]])  # Example input
-        
-        # Normalize the input data using the same scaler used during training
-        sample_input = scaler.transform(sample_input)
-        
-        # Reshape the input data to match the model's expected input shape
-        sample_input = sample_input.reshape((1, sample_input.shape[1], 1))
-        
-        # Convert the numpy array to a JSON-serializable list
-        input_data = sample_input.tolist()
-        
-        # Prepare the data payload for the request
-        data = json.dumps({
-            "signature_name": "serving_default",  # Use the default signature
-            "instances": input_data
-        })
-        
-        # Send the POST request to the TensorFlow Serving API
-        headers = {"content-type": "application/json"}
-        response = requests.post(url, data=data, headers=headers)
-        
-        # Check if the response status code is not 200
-        if response.status_code != 200:
-            raise ValueError(f"Received response code {response.status_code}: {response.text}")
-        
-        # Parse the JSON response
-        predictions = json.loads(response.text)['predictions']
-        
-        # Interpret the predictions
-        predicted_class, predicted_attendance_level = interpret_predictions(predictions)
-        
-        return predicted_class, predicted_attendance_level
-    except Exception as e:
-        logging.error(f"Error in get_prediction: {str(e)}")
-        raise e
+from prediction import get_prediction
+from scaler import scaler
+from datetime import datetime, timedelta
 
 # Initialize the Flask application
 app = Flask(__name__)
 
-# Initialize the scaler (use the same scaler used during training)
-# Assume you have access to the original training data for initializing the scaler
-X_train = np.array([[0, 3, 15, 0, 1], [1, 3, 16, 0, 1], [2, 3, 17, 0, 1], [3, 3, 18, 0, 1], [4, 3, 19, 0, 1], [5, 3, 20, 1, 1], [6, 3, 21, 1, 1]])
-scaler = StandardScaler().fit(X_train)
+# Function to determine if a given date is a weekend
+def is_weekend(date):
+    return date.weekday() >= 5
+
+# Function to determine if a given date is a special event (placeholder logic)
+def is_special_event(date):
+    special_events_dates = [(7, 4), (12, 25),(4,23),(12,9),(8,5),(3,6),(11,27),(3,10),(7,26)]  # Example: 4th of July, Christmas
+    return 1 if (date.month, date.day) in special_events_dates else 0
 
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        data = request.get_json(force=True)
-        day_of_week = int(data['Day_of_Week'])
-        month = int(data['Month'])
-        day_of_month = int(data['Day_of_month'])
-        is_weekend = int(data['Is_Weekend'])
-        special_event = int(data['Special_Event'])
+        # Get current date
+        current_date = datetime.now()
+        
+        # Extract day of the week, month, and day of the month
+        day_of_week = current_date.weekday()  # Monday is 0 and Sunday is 6
+        month = current_date.month
+        day_of_month = current_date.day
+        weekend = is_weekend(current_date)
+        special_event = is_special_event(current_date)
+         # Set factor based on special event
+        factor = 1.5 if special_event else 1.0
 
-        predicted_class, predicted_attendance_level = get_prediction(day_of_week, month, day_of_month, is_weekend, special_event, scaler)
+        predicted_class, predicted_attendance_level = get_prediction(day_of_week, month, day_of_month, weekend, special_event, scaler, factor)
         
         return jsonify({
             'Day_of_Week': day_of_week,
             'Month': month,
             'Day_of_month': day_of_month,
-            'Is_Weekend': is_weekend,
+            'Is_Weekend': weekend,
             'Special_Event': special_event,
             'Predicted_Class': predicted_class,
             'Predicted_Attendance_Level': predicted_attendance_level
         })
     except Exception as e:
         logging.error(f"Error in predict endpoint: {str(e)}")
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/predict_week', methods=['POST'])
+def predict_week():
+    try:
+        # Get the current date
+        current_date = datetime.now()
+        
+        # Find the upcoming Monday
+        start_date = current_date + timedelta(days=(0 - current_date.weekday()))
+        
+        predictions = []
+        for i in range(7):
+            # Calculate the date for each day of the week
+            date = start_date + timedelta(days=i)
+            
+            # Extract day of the week, month, and day of the month
+            day_of_week = date.weekday()
+            month = date.month
+            day_of_month = date.day
+            weekend = is_weekend(date)
+            special_event = is_special_event(date)
+            
+           # Set factor based on special event
+            factor = 1.5 if special_event else 1.0
+            
+            # Get prediction
+            predicted_class, predicted_attendance_level = get_prediction(day_of_week, month, day_of_month, weekend, special_event, scaler, factor)
+            
+            # Append the results
+            predictions.append({
+                'Date': date.strftime('%Y-%m-%d'),
+                'Day_of_Week': day_of_week,
+                'Month': month,
+                'Day_of_month': day_of_month,
+                'Is_Weekend': weekend,
+                'Special_Event': special_event,
+                'Predicted_Class': predicted_class,
+                'Predicted_Attendance_Level': predicted_attendance_level
+            })
+        
+        return jsonify(predictions)
+    except Exception as e:
+        logging.error(f"Error in predict_week endpoint: {str(e)}")
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
