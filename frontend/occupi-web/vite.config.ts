512752cd3add0c27941260909ba44d6b4676b3c5
Michael-u21546551 <u21546551@tuks.co.za>
2024-07-28 11:21:08 +0200

    chore: Update Vite config to handle HTTPS certificates dynamically

--- frontend/occupi-web/vite.config.ts
+++ frontend/occupi-web/vite.config.ts
@@ -3,6 +3,19 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import tsconfigPaths from "vite-tsconfig-paths";
 import mkcert from "vite-plugin-mkcert";
+import fs from 'fs';
+
+// Define paths to the certificates
+let keyPath = '';
+let certPath = '';
+
+if (process.env.NODE_ENV === 'preview-dev') {
+  keyPath = '/etc/letsencrypt/live/dev.occupi.tech/privkey.pem';
+  certPath = '/etc/letsencrypt/live/dev.occupi.tech/fullchain.pem';
+} else if (process.env.NODE_ENV === 'preview-prod') {
+  keyPath = '/etc/letsencrypt/live/app.occupi.tech/privkey.pem';
+  certPath = '/etc/letsencrypt/live/app.occupi.tech/fullchain.pem';
+}
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), mkcert()],
@@ -39,6 +52,12 @@ export default defineConfig({
         secure: false,
         rewrite: (path) => path.replace(/^\/auth/, '/auth'),
       }
-    }
+    },
+    https: process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'preview' || process.env.NODE_ENV === 'development' ? {} : {
+      key: fs.readFileSync(keyPath),
+      cert: fs.readFileSync(certPath),
+    },
+    host: '0.0.0.0',
+    port: 4173,
   },
 });
