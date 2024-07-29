9678a176673ddec11a3d9edf8f61a649543ab10b
cmokou <klifo14@gmail.com>
2024-07-29 08:59:51 +0200

    refactor: Update VisualFeatures component with new images and descriptions

--- frontend/occupi/components/ui/VisualFeatures/VisualFeatures.jsx
+++ frontend/occupi/components/ui/VisualFeatures/VisualFeatures.jsx
@@ -77,10 +77,12 @@ const VisualFeatures = () => {
           <h2 className="text-gray-50 text-3xl font-semibold sm:text-4xl mt-10">
             Make AI-powered predictions for office capacity
           </h2>
-          <p className="mt-3">
+          <p className="mt-3 mb-10">
             By participating in Occupi, you're directly contributing to the
-            growth of artificial intelligence. Networks like these are required
-            to train AI models, and AI labs are some of our biggest customers.
+            growth of artificial intelligence. By continuously improving
+            predictive accuracy, the Occupi system introduces a dynamic,
+            data-driven approach to office capacity management, benefiting both
+            daily users and office operations.
           </p>
         </div>
       </div>
@@ -90,111 +92,116 @@ const VisualFeatures = () => {
           <div className="w-full md:w-1/2 px-4">
             <div className="max-w-xl mx-auto text-center">
               <h2 className="text-gray-50 text-3xl font-semibold sm:text-4xl mt-10">
-                Use Occupi Web or Desktop for your Analytics
+                Use Occupi Web for your Analytics.
               </h2>
-              
+              <p className="mt-3">
+                It offers real-time capacity status to employees and visitors,
+                and detailed future occupancy predictions to office managers and
+                owners, enabling optimized space usage and improved efficiency.
+              </p>
             </div>
-            
           </div>
           <div className="pl-8 mt-10">
-              <Image
-                            src={Frame8}
-                            alt="Occupi App Screenshot"
-                            width={550}
-                            height={100}
-                            className="rounded-lg shadow-lg"
-                        />
-                </div>
-        </div>
-      </div>
-
-      <div className="custom-screen text-gray-300">
-        <div className="flex flex-wrap">
-        <div className="pl-8">
-              <Image
-                            src={Frame6}
-                            alt="Occupi App Screenshot"
-                            width={550}
-                            height={100}
-                            className="rounded-lg shadow-lg"
-                        />
-                </div>
-          <div className="w-full md:w-1/2 px-4">
-            <div className="max-w-xl mx-auto text-center">
-              <h2 className="text-gray-50 text-3xl font-semibold sm:text-4xl mt-10">
-                Use Occupi Web or Desktop for your Analytics
-              </h2>
-              
-            </div>
-            
+            <Image
+              src={Frame8}
+              alt="Occupi App Screenshot"
+              width={550}
+              height={100}
+              className="rounded-lg shadow-lg"
+            />
           </div>
-          
         </div>
       </div>
 
       <div className="custom-screen text-gray-300">
         <div className="flex flex-wrap">
-        
+          <div className="pl-8">
+            <Image
+              src={Frame6}
+              alt="Occupi App Screenshot"
+              width={550}
+              height={100}
+              className="rounded-lg shadow-lg"
+            />
+          </div>
           <div className="w-full md:w-1/2 px-4">
             <div className="max-w-xl mx-auto text-center">
               <h2 className="text-gray-50 text-3xl font-semibold sm:text-4xl mt-10">
-                Use Occupi Web or Desktop for your Analytics
+                Use Occupi Desktop for more detailed prediction output.
               </h2>
-              
+              <p className="mt-3">
+                Leveraging historical data and real-time inputs, the system
+                helps office managers make informed decisions about space
+                utilization, enhancing both immediate and long-term planning.
+              </p>
             </div>
-            
+          </div>
+        </div>
+      </div>
+
+      <div className="custom-screen text-gray-300">
+        <div className="flex flex-wrap">
+          <div className="w-full md:w-1/2 px-4">
+            <div className="max-w-xl mx-auto text-center">
+              <h2 className="text-gray-50 text-3xl font-semibold sm:text-4xl mt-10">
+                Who are we?
+              </h2>
+              <p className="pl-8 mt-12">
+                Y2Kode is a team of driven individuals with a passion for
+                technology and a commitment to excellence. We are excited about
+                the idea behind the Office Capacity Predictor and appreciate the
+                freedom to infuse it with our unique Y2Kode twist.
+              </p>
+            </div>
           </div>
           <div className="pl-8">
-              <Image
-                            src={Frame5}
-                            alt="Occupi App Screenshot"
-                            width={550}
-                            height={100}
-                            className="rounded-lg shadow-lg"
-                        />
-                </div>
+            <Image
+              src={Frame5}
+              alt="Occupi App Screenshot"
+              width={550}
+              height={100}
+              className="rounded-lg shadow-lg"
+            />
+          </div>
         </div>
       </div>
 
       <div className="custom-screen text-gray-300">
         <div className="max-w-xl mx-auto text-center">
           <h2 className="text-gray-50 text-3xl font-semibold sm:text-4xl mt-10">
-            Make AI-powered predictions for office capacity
+            The Occupi Office Capacity Prediction system revolutionizes office
+            space management by utilizing advanced machine learning algorithms
+            and predictive models.
           </h2>
           <p className="mt-3">
-            By participating in Occupi, you're directly contributing to the
-            growth of artificial intelligence. Networks like these are required
-            to train AI models, and AI labs are some of our biggest customers.
+            As Y2Kode, we have developed this innovative solution for mobile for
+            everyday office users, as well as a web application, and a desktop
+            application for providing managerial services and delivering more
+            detailed predictive outputs.
           </p>
-           <div className="pl-8">
-              <Image
-                            src={Frame7}
-                            alt="Occupi App Screenshot"
-                            width={550}
-                            height={100}
-                            className="rounded-lg shadow-lg mt-10"
-                        />
-                </div>
-
+          <div className="pl-8">
+            <Image
+              src={Frame7}
+              alt="Occupi App Screenshot"
+              width={550}
+              height={100}
+              className="rounded-lg shadow-lg mt-10"
+            />
+          </div>
         </div>
-       
-
-       
       </div>
 
-      
-      
       <div className="custom-screen text-gray-300">
         <div className="max-w-xl mx-auto text-center">
-        <div className="w-full mt-20">
-    <Image
-        src={Frame3}
-        alt="Occupi App Screenshot"
-        width={850}
-        height={100}
-        className="w-full rounded-lg shadow-lg"
-    />
-</div>
+          <div className="w-full mt-20">
+            <Image
+              src={Frame3}
+              alt="Occupi App Screenshot"
+              width={850}
+              height={100}
+              className="w-full rounded-lg shadow-lg"
+            />
+          </div>
           <div className="flex justify-center items-center mt-6 space-x-4 ml-20">
             <Link
               href="https://play.google.com/store"
