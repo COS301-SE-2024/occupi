4ea965b41f3c5732d7b79c434f07ceba109e131a
cmokou <klifo14@gmail.com>
2024-07-30 02:23:07 +0200

    chore: Mock NavBarProvider in NavBar-test.tsx

--- frontend/occupi-mobile4/components/__tests__/NavBar-test.tsx
+++ frontend/occupi-mobile4/components/__tests__/NavBar-test.tsx
@@ -1,9 +1,23 @@
 import * as React from 'react';
 import renderer from 'react-test-renderer';
 import NavBar from '../NavBar';
+import { useNavBar } from '../NavBarProvider';
 
-it(`renders correctly`, () => {
-  const tree = renderer.create(<NavBar>Snapshot test!</NavBar>).toJSON();
+// Mock the NavBarProvider module
+jest.mock('../NavBarProvider', () => ({
+  useNavBar: jest.fn(),
+}));
 
-  expect(tree).toMatchSnapshot();
+describe('NavBar', () => {
+  it(`renders correctly`, () => {
+    // Mock the useNavBar hook implementation
+    (useNavBar as jest.Mock).mockReturnValue({
+      currentTab: 'Home',
+      setCurrentTab: jest.fn(),
+    });
+
+    const tree = renderer.create(<NavBar />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
 });
