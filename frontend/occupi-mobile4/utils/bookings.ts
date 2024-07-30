18f440b64bc26d55b6ee51edd5b1e7b978aeacb3
Kamogelo Moeketse <kamogelomoeketse@gmail.com>
2024-07-30 04:00:45 +0200

    chore: check-in refactor

--- frontend/occupi-mobile4/utils/bookings.ts
+++ frontend/occupi-mobile4/utils/bookings.ts
@@ -1,5 +1,5 @@
 import { Booking } from "@/models/data";
-import { getUserBookings } from "../services/apiservices";
+import { checkin, getUserBookings } from "../services/apiservices";
 import * as SecureStore from 'expo-secure-store';
 
 export async function fetchUserBookings(): Promise<Booking[]> {
@@ -18,5 +18,26 @@ export async function fetchUserBookings(): Promise<Booking[]> {
     } catch (error) {
         console.error('Error:', error);
         throw error; // Add a throw statement to handle the error case
+    }
+}
+
+export async function userCheckin() {
+    let roomstring = await SecureStore.getItemAsync("CurrentRoom");
+    const room = JSON.parse(roomstring as string);
+    const bookingId = room?.occupiId;
+    let email = await SecureStore.getItemAsync('Email');
+    const body = {
+        email: email as string,
+        bookingId: bookingId
+    }
+    try {
+        const response = await checkin(body);
+        if (response.status === 200) {
+            return response.message;
+        }
+        return response.message;
+    } catch (error) {
+        console.error('Error:', error);
+        throw error;
     }
 }
