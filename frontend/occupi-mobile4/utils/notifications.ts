00482e915154cbda8a875769ba1031dc2b5ef47f
cmokou <klifo14@gmail.com>
2024-08-22 12:03:21 +0200

    100% test BUT uncovered lines

--- frontend/occupi-mobile4/utils/notifications.ts
+++ frontend/occupi-mobile4/utils/notifications.ts
@@ -32,10 +32,8 @@ export async function registerForPushNotificationsAsync() {
   let token;
 
   if (!Device.isDevice) {
- 
-    
     global.alert('Must use physical device for Push Notifications');
-    return;
+    return undefined;
   }
 
   if (Platform.OS === 'android') {
@@ -47,38 +45,33 @@ export async function registerForPushNotificationsAsync() {
     });
   }
 
-  if (Device.isDevice) {
-    const { status: existingStatus } = await Notifications.getPermissionsAsync();
-    let finalStatus = existingStatus;
-    if (existingStatus !== 'granted') {
-      const { status } = await Notifications.requestPermissionsAsync();
-      finalStatus = status;
+  const { status: existingStatus } = await Notifications.getPermissionsAsync();
+  let finalStatus = existingStatus;
+  if (existingStatus !== 'granted') {
+    const { status } = await Notifications.requestPermissionsAsync();
+    finalStatus = status;
+  }
+  if (finalStatus !== 'granted') {
+    alert('Failed to get push token for push notification!');
+    return undefined;
+  }
+  try {
+    const projectId =
+      Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
+    if (!projectId) {
+      throw new Error('Project ID not found');
     }
-    if (finalStatus !== 'granted') {
-      alert('Failed to get push token for push notification!');
-      return;
-    }
-    try {
-      const projectId =
-        Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
-      if (!projectId) {
-        throw new Error('Project ID not found');
-      }
-      token = (
-        await Notifications.getExpoPushTokenAsync({
-          projectId,
-        })
-      ).data;
-    } catch (e) {
-      token = `${e}`;
-    }
-  } else {
-    alert('Must use physical device for Push Notifications');
+    token = (
+      await Notifications.getExpoPushTokenAsync({
+        projectId,
+      })
+    ).data;
+  } catch (e) {
+    token = `${e}`;
   }
 
   return token;
 }
-
 
  export async function sendPushNotification(expoPushTokens: string[], title: string, body: string) {
   const messages = expoPushTokens.map(token => ({
