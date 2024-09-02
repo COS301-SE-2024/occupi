b7d867febe86d210231e343f060c6e404b3af3ef
Kamogelo Moeketse <kamogelomoeketse@gmail.com>
2024-09-03 01:12:56 +0200

    chore: Add functions for date and week predictions in aimodel.ts

--- frontend/occupi-mobile4/services/aimodel.ts
+++ frontend/occupi-mobile4/services/aimodel.ts
@@ -41,6 +41,47 @@ export async function getDayPredictions(): Promise<Prediction | undefined> {
     return undefined;
 }
 
+export async function getDatePredictions(date: string): Promise<Prediction | undefined> {
+    // let authToken = await SecureStore.getItemAsync('Token');
+    try {
+        const response = await axios.get(`https://ai.occupi.tech/predict_date?date=${date}`, {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+        });
+        return response.data as Prediction;
+    } catch (error) {
+        console.error(`Error in ${Function}:`, error);
+        if (axios.isAxiosError(error) && error.response?.data) {
+            return error.response.data;
+        }
+    }
+
+    return undefined;
+}
+
+export async function getWeekPredictions(date: string): Promise<Prediction[] | undefined> {
+    // let authToken = await SecureStore.getItemAsync('Token');
+    try {
+        const response = await axios.get(`https://ai.occupi.tech/predict_week_from_date?date=${date}`, {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+        });
+        return response.data as Prediction[];
+    } catch (error) {
+        console.error(`Error in ${Function}:`, error);
+        if (axios.isAxiosError(error) && error.response?.data) {
+            return error.response.data;
+        }
+    }
+
+    return undefined;
+}
+
+
 
 export { Prediction };
 // getPredictions();
--- frontend/occupi-mobile4/services/apiservices.ts
+++ frontend/occupi-mobile4/services/apiservices.ts
@@ -438,3 +438,4 @@ export async function updateNotificationSettings(req: NotificationSettingsReq): 
     } as Unsuccessful;
   }
 }
+
