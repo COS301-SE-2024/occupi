33f4665e6348ffe05a2ca7e678490d5928d6e036
Michael-u21546551 <u21546551@tuks.co.za>
2024-09-23 22:40:13 +0200

    Refactor Redis cache setup and router endpoints
    - Refactored the CreateCache and CreateMobileCache functions in setup.go to include error handling for Redis connection and print error messages.
    - Added a DELETE endpoint for deleting notifications in router.go.

--- occupi-backend/configs/setup.go
+++ occupi-backend/configs/setup.go
@@ -88,10 +88,18 @@ func CreateCache() *redis.Client {
 	url := fmt.Sprintf("redis://%s:%s@%s:%s/0?protocol=3", redisUsername, redisPassword, redisHost, redisPort)
 	opts, err := redis.ParseURL(url)
 	if err != nil {
+		fmt.Println(err)
 		logrus.Fatal(err)
 	}
 
 	client := redis.NewClient(opts)
+
+	// Test connection
+	err = client.Ping(context.Background()).Err()
+	if err != nil {
+		fmt.Println("could not connect to Redis: ", err)
+		logrus.Fatalf("could not connect to Redis: %v", err)
+	}
 
 	fmt.Println("Cache created!")
 	logrus.Info("Cache created!")
@@ -109,13 +117,21 @@ func CreateMobileCache() *redis.Client {
 	url := fmt.Sprintf("redis://%s:%s@%s:%s/0?protocol=3", redisUsername, redisPassword, redisHost, redisPort)
 	opts, err := redis.ParseURL(url)
 	if err != nil {
+		fmt.Println(err)
 		logrus.Fatal(err)
 	}
 
 	client := redis.NewClient(opts)
 
-	fmt.Println("Cache created!")
-	logrus.Info("Cache created!")
+	// Test connection
+	err = client.Ping(context.Background()).Err()
+	if err != nil {
+		fmt.Println("could not connect to mobile Redis: ", err)
+		logrus.Fatalf("could not connect to mobile Redis: %v", err)
+	}
+
+	fmt.Println("Mobile Cache created!")
+	logrus.Info("Mobile Cache created!")
 
 	return client
 }
