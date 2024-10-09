f7a305592f713b9401fb81b6506c6f4049fb14be
Michael-u21546551 <u21546551@tuks.co.za>
2024-10-09 12:34:24 +0200

    Add Reload icon component

---
+++ frontend/occupi-web/src/assets/icons/Reload.tsx
@@ -0,0 +1,9 @@
+const Reload = () => {
+    return (
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M4.87115 14.5C5.88914 17.6939 8.80463 20 12.2424 20C16.5268 20 20 16.4183 20 12C20 7.58172 16.5268 4 12.2424 4C9.37103 4 6.86399 5.60879 5.52267 8M4.87115 7.21845L5.93971 8.25008M7.87879 9H4V5L7.87879 9Z" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+    )
+}
+
+export default Reload
