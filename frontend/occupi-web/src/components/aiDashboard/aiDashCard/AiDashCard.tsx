26d34db7a5366fc4a9756f05b3127123e5d90e39
Tinashe-Austin <tinashe.austin@tuks.co.za>
2024-10-20 22:25:55 +0200

    Refactor styles in AiDashCard component Refactor styles in GraphContainer component

--- frontend/occupi-web/src/components/aiDashboard/aiDashCard/AiDashCard.tsx
+++ frontend/occupi-web/src/components/aiDashboard/aiDashCard/AiDashCard.tsx
@@ -27,13 +27,13 @@ const AiDashCard: React.FC<AiDashCardProps> = ({
 }) => {
   return (
     <Card
-      className="w-full max-w-sm h-auto text-text_col shadow-lg rounded-xl overflow-hidden transition-all duration-300 hover:shadow-xl"
+      className="w-full max-w-sm h-auto shadow-lg rounded-xl overflow-hidden transition-all duration-300 hover:shadow-xl"
       data-testid="ai-dash-card">
       <CardHeader
-        className="flex text-text_col justify-between items-center p-4 bg-secondary dark:bg-zinc-500"
+        className="flex justify-between items-center p-4 bg-secondary dark:bg-zinc-500"
         data-testid="card-header">
         <div className="flex items-center gap-3">
-          <div className="text-2xl" data-testid="icon">
+          <div className="text-2xl text-text_col " data-testid="icon">
             {icon}
           </div>
           <h4 className="text-lg font-semibold" data-testid="title">
@@ -43,13 +43,13 @@ const AiDashCard: React.FC<AiDashCardProps> = ({
         <Button
           onClick={onRemove}
           size="sm"
-          className="bg-transparent text-2xl  hover:bg-gray-200 dark:hover:bg-gray-600 text-gray-600 dark:text-gray-300"
+          className="bg-transparent text-2xl  hover:bg-red_salmon text-gray-600 dark:text-gray-300"
           data-testid="remove-button">
          ×
         </Button>
       </CardHeader>
       <CardBody
-        className="flex  flex-col items-center justify-center py-6"
+        className="    background-color: hsl(var(--nextui-content1) / var(--nextui-content1-opacity, var(--tw-bg-opacity)));  flex flex-col items-center justify-center py-6"
         data-testid="card-body">
         <h2
           className="text-6xl font-bold text-text_col -mb-2"
