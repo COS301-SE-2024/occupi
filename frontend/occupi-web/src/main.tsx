e682794527a716e0e78598ca3239f2fe1b4e55d3
Michael-u21546551 <u21546551@tuks.co.za>
2024-10-21 10:35:29 +0200

    removing faro for dev to minimize noise in console.log

--- frontend/occupi-web/src/main.tsx
+++ frontend/occupi-web/src/main.tsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import {NextUIProvider} from "@nextui-org/react";
-import { initFaro } from 'instrumentation.ts';
+//import { initFaro } from 'instrumentation.ts';
 import TimeAgo from 'javascript-time-ago'
 
 import en from 'javascript-time-ago/locale/en'
 
-initFaro();
+//initFaro();
 TimeAgo.addDefaultLocale(en)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
