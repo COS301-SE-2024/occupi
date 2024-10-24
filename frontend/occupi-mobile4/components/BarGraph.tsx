9aa0a009bccb902717f4fe42d6ad5ffcfafe14f8
Kamogelo Moeketse <kamogelomoeketse@gmail.com>
2024-10-24 11:33:26 +0200

    Refactor BarGraph component to remove commented out code and adjust animation durations

--- frontend/occupi-mobile4/components/BarGraph.tsx
+++ frontend/occupi-mobile4/components/BarGraph.tsx
@@ -42,9 +42,9 @@ const BarGraph = ({data,tab}) => {
           maxValue={1500}
           noOfSections={5}
           // hideRules
-        //   animateOnDataChange={true}
+          // animateOnDataChange={true}
           animationDuration={500}
-        //   onDataChangeAnimationDuration={100}
+          // onDataChangeAnimationDuration={100}
           endSpacing={0}
           yAxisTextStyle={{color: labels}}
           xAxisLabelTextStyle={{color: labels}}
@@ -53,8 +53,8 @@ const BarGraph = ({data,tab}) => {
           hideRules
           frontColor={currentTheme === 'dark' ? "lightgray" : "darkgrey"}
           gradientColor={accentColour}
-        //   barBorderTopLeftRadius={5}
-        //   barBorderTopRightRadius={5}
+          // barBorderTopLeftRadius={5}
+          // barBorderTopRightRadius={5}
           spacing={12}
           backgroundColor={currentTheme === 'dark' ? "transparent" : "white"}
           // showVerticalLines
